a8f834c2782cdf8e27feb2b6b3540cce6ea3869f
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-04-30 23:28:22 +0200

    fixing it... WIP

--- sampleScripts/linux-mpi-debug.sh
+++ sampleScripts/linux-mpi-debug.sh
@@ -3,10 +3,12 @@
 #
 
 EXTRA_ARGS=$@
+
 cmake \
--DTribitsExProj_TRIBITS_DIR=$TRIBITS_DIR \
--DTPL_ENABLE_MPI=ON \
--DTribitsExProj_ENABLE_Fortran=OFF \
--DTribitsExProj_ENABLE_TESTS=ON \
-$EXTRA_ARGS \
-/Users/fnrizzi/Desktop/test/pack
+    -DTPL_ENABLE_MPI=ON \
+    -DTribitsExProj_ENABLE_Fortran=OFF \
+    -DTribitsExProj_ENABLE_TESTS=ON \
+    $EXTRA_ARGS \
+    /Users/fnrizzi/Desktop/rompp
+
+#    -DTribitsExProj_TRIBITS_DIR=$TRIBITS_DIR \
