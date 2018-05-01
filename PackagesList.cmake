707c8c8830502521f2ce9b90e839d39d032ac8d4
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-05-01 13:44:38 +0200

    adding tribits structure for code & packages. WIP.

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -1,10 +1,12 @@
 TRIBITS_REPOSITORY_DEFINE_PACKAGES(
-  SimpleCxx          packages/simple_cxx         PT
-  MixedLang          packages/mixed_lang         PT
-  InsertedPkg        InsertedPkg                 ST
-  WithSubpackages    packages/with_subpackages   PT
-  WrapExternal       packages/wrap_external      ST
+  TimeIntegrator     packages/time_integrator   PT
+  #SimpleCxx          packages/simple_cxx        PT
+  #MixedLang          packages/mixed_lang         PT
+  #InsertedPkg        InsertedPkg                 ST
+  #WithSubpackages    packages/with_subpackages   PT
+  #WrapExternal       packages/wrap_external      ST
   )
 
-TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(WrapExternal Windows)
-TRIBITS_ALLOW_MISSING_EXTERNAL_PACKAGES(InsertedPkg)
+
+#TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(WrapExternal Windows)
+#TRIBITS_ALLOW_MISSING_EXTERNAL_PACKAGES(InsertedPkg)
