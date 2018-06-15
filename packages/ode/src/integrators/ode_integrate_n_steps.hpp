c14848e0ea6dfcd43e1f3c84e797c907634d39ce
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-06-15 02:05:24 +0200

    committing integrateNsteps and forward declarations

--- packages/ode/src/integrators/ode_integrate_n_steps.hpp
+++ packages/ode/src/integrators/ode_integrate_n_steps.hpp
@@ -3,89 +3,60 @@
 #define ODE_INTEGRATE_N_STEPS_HPP_
 
 #include "ode_ConfigDefs.hpp"
-#include "ode_forward_declarations.hpp"
-#include "ode_void_collector.hpp"
+#include "ode_integrate_n_steps_impl.hpp"
+
 
 namespace ode{
-
-  template<typename stepper_type,
-  	   typename state_type,
-  	   typename time_type,
-  	   typename n_steps_type,
-  	   typename collector_type
-  	   >
-  typename std::enable_if<!std::is_void<stepper_type>::value &&
-			  std::is_integral<n_steps_type>::value
-  			  >::type
-  integrateNStepsImpl(stepper_type & stepper,
-  		      state_type & stateIn,
-  		      time_type start_time,
-  		      time_type dt,
-  		      n_steps_type num_of_steps,
-  		      typename std::conditional< std::is_same<collector_type, ode::voidCollector>::value,
-		      ode::voidCollector, collector_type &>::type collector)
-  {
-    ode::details::time_type time = start_time;
-    size_t step = 0;
-    for( ; step < num_of_steps ; ++step)
-    {
-      // call collector/observer
-      collector(step, time, stateIn);
-      // do one step
-      stepper.doStep(stateIn, time, dt);
-      // advance time
-      time += dt; //change this so that we do multiply rather than summing
-    }
-    collector(step, time, stateIn);
-  }
-
-
-
-
-  /*+++++++++++++++++++++++++++++++++++++++++++++++++++++
-    TODO: separate interface and impl
-  +++++++++++++++++++++++++++++++++++++++++++++++++++++*/  
-
-  template<typename stepper_type,
-	   typename state_type,
-	   typename collector_functor_type
-	   >
-  typename std::enable_if< !std::is_same<stepper_type,void>::value >::type
-  integrateNSteps(stepper_type & stepper,
-		  state_type & stateIn,
-		  collector_functor_type & collector,
-		  ode::details::time_type start_time,
-		  ode::details::time_type dt,
-		  size_t num_steps)
-  {
-    integrateNStepsImpl<stepper_type, state_type,
-      			ode::details::time_type, size_t,
-      			collector_functor_type>(stepper, stateIn,
-						start_time, dt,
-						num_steps, collector);
-  }
-
-
   
-  template<typename stepper_type,
-	   typename state_type
-	   >
-  typename std::enable_if< !std::is_same<stepper_type,void>::value >::type
-  integrateNSteps(stepper_type & stepper,
-		  state_type & stateIn,
-		  ode::details::time_type start_time,
-		  ode::details::time_type dt,
-		  size_t num_steps)
-  {
-    integrateNStepsImpl<stepper_type, state_type,
-      			ode::details::time_type, size_t,
-			ode::voidCollector>(stepper, start_time, dt,
-					    num_steps, ode::voidCollector());
-  }
+template<typename stepper_type,
+	 typename state_type,
+	 typename collector_functor_type,
+	 typename time_type,
+	 typename std::enable_if<
+	   !std::is_void<stepper_type>::value &&
+	   !std::is_void<collector_functor_type>::value && 
+	   !std::is_same<collector_functor_type,
+			 voidCollector>::value
+	   >::type * = nullptr
+	 >
+void integrateNSteps(stepper_type & stepper,
+		     state_type & stateIn,
+		     collector_functor_type & collector, 
+		     time_type start_time,
+		     time_type dt,
+		     size_t num_steps)
+{
+  impl::integrateNStepsImpl<stepper_type, state_type,
+			time_type, collector_functor_type>
+    (stepper,stateIn,start_time, dt,num_steps, collector);
+}
 
-  
+
+//----------------------------------------------------------------
+
+
+template<typename stepper_type,
+	 typename state_type,
+	 typename time_type,
+	 typename std::enable_if<
+	   !std::is_void<stepper_type>::value
+	   >::type * = nullptr
+	 >
+void integrateNSteps(stepper_type & stepper,
+		     state_type & stateIn,
+		     time_type start_time,
+		     time_type dt,
+		     size_t num_steps)
+{
+  impl::integrateNStepsImpl<stepper_type, state_type,
+			    time_type>
+    (stepper, stateIn, start_time, dt, num_steps);
+}
+
+
+
+
 }//end namespace
-
 #endif 
 
 
@@ -108,11 +79,11 @@ namespace ode{
 // integrateNStepsImpl(stepper_type & stepper,
 // 		  state_type & stateIn,
 // 		  collector_functor_type & collector,
-// 		  ode::details::time_type start_time,
-// 		  ode::details::time_type dt,
+// 		  time_type start_time,
+// 		  time_type dt,
 // 		  size_t num_steps)
 // {
-//   ode::details::time_type time = start_time;
+//   time_type time = start_time;
 //   size_t step = 0;
 //   for( ; step < num_steps ; ++step)
 //   {
