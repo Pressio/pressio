fca8d6742b7c7a38947aacf0bfcf0e39598506d1
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-06-18 16:50:55 +0200

    removing comments that pollute

--- packages/core/src/core_forward_declarations.hpp
+++ packages/core/src/core_forward_declarations.hpp
@@ -7,16 +7,11 @@
 namespace core {
 
 
-//***************************************
-// forward declaration of vector class
-//***************************************
 template <typename wrapped_type,
 	  typename Enable = void>
 class vector;
 
-//***************************************
-// forward declaration of matrix class
-//***************************************
+
 template <typename wrapped_type,
 	  typename Enable = void>
 class matrix;
