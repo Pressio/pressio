2e4e0049fd347ee54f2aa27aa8cd6db8b27da3f6
Giulio Borghesi <gborghe@sandia.gov>
2018-07-02 14:44:38 -0700

    Added factory class for creating linear iterative solvers

---
+++ packages/solvers/src/experimental/solvers_traits.hpp
@@ -0,0 +1,62 @@
+
+#ifndef _SOLVERS_TRAITS_HPP_
+#define _SOLVERS_TRAITS_HPP_
+
+namespace solvers {
+
+// Iterative solvers types
+struct CG {};
+struct Gmres {};
+struct Bicgstab {};
+
+// Preconditioner types
+struct Jacobi {};
+
+
+namespace details {
+
+// Solvers traits
+template <typename T>
+struct solvers_traits {
+  static constexpr bool eigen_enabled = false;
+  static constexpr bool trilinos_enabled = false;
+};
+
+template <>
+struct solver_traits<CG> {
+
+  template <typename T>
+  using eigen_solver_type = Eigen::ConjugateGradient<T>;
+
+  static constexpr bool eigen_enabled = true;
+  static constexpr bool trilinos_enabled = true;
+};
+
+template <>
+struct solver_traits<Bicgstab> {
+
+  template <typename T>
+  using eigen_solver_type = Eigen::BiCGSTAB<T>;
+
+  static constexpr bool eigen_enabled = true;
+  static constexpr bool trilinos_enabled = true;
+};
+
+
+// Preconditioners traits 
+template <typename T>
+struct preconditioner_traits {
+  static constexpr bool eigen_enabled = false;
+  static constexpr bool trilinos_enabled = false;
+};
+
+template <>
+struct preconditioner_traits<Jacobi> {
+  static constexpr bool eigen_enabled = false;
+  static constexpr bool trilinos_enabled = true;
+};
+
+} // end namespace details
+} // end namespace solvers
+
+#endif
