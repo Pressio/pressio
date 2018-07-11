d433d06663955af9525610c66a50aa22bbe781a4
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-07-11 18:17:43 +0200

    fixed small bug in the traits for explicit euler stepper

--- packages/ode/src/steppers/explicit_steppers/ode_explicit_stepper_traits.hpp
+++ packages/ode/src/steppers/explicit_steppers/ode_explicit_stepper_traits.hpp
@@ -32,7 +32,7 @@ struct traits<impl::explicitEulerStepperImpl<state_type,
   using sizer_t = sizer_type;
   using residual_policy_t = residual_policy_type;
 
-  static constexpr bool advanceIncrement = true;//residual_policy_t::advanceIncrement;
+  static constexpr bool advanceIncrement = residual_policy_t::advanceIncrement;
   
   using order_t = unsigned int;
   static constexpr order_t order_value = 1;    
@@ -75,52 +75,7 @@ struct traits<impl::explicitRungeKutta4StepperImpl<state_type,
   static constexpr order_t order_value = 4;
 };
 
-
   
 }//end namespace details
 }//end namespace ode
-
 #endif
-
-
-
-
-  
-  
-// template<typename state_type,
-// 	 typename residual_type,
-// 	 typename jacobian_type,
-// 	 typename scalar_type,
-// 	 typename model_type,
-// 	 typename time_type,
-// 	 typename solver_policy_type,
-// 	 typename residual_policy_type,
-// 	 typename jacobian_policy_type>
-// struct traits< implicitEulerStepper<state_type, residual_type,
-// 				    jacobian_type, scalar_type,
-// 				    model_type, time_type,
-// 				    solver_policy_type,
-// 				    residual_policy_type,
-// 				    jacobian_policy_type>>
-// {
-//   using stepper_t = implicitEulerStepper<state_type, residual_type,
-// 					 jacobian_type,
-// 					 scalar_type, model_type,
-// 					 time_type,
-// 					 solver_policy_type,
-// 					 residual_policy_type,
-// 					 jacobian_policy_type>;
-//   using state_t =  state_type;
-//   using residual_t = residual_type;
-//   using jacobian_t =  jacobian_type;
-//   using scalar_t = scalar_type;    
-//   using model_t = model_type;
-//   using time_t = time_type;
-//   using solver_policy_t = solver_policy_type;
-//   using residual_policy_t = residual_policy_type;
-//   using jacobian_policy_t = jacobian_policy_type;
-
-//   using order_t = unsigned int;
-//   static constexpr order_t order_value = 1;
-// };
-
