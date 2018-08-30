8a2de84675d57d70c5f1c84ab6473e64d12c4bc2
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-08-30 12:50:48 +0000

    Update config-mpi-debug.sh

--- sampleScripts/config-mpi-debug.sh
+++ sampleScripts/config-mpi-debug.sh
@@ -37,8 +37,8 @@ cmake \
     -D rompp_ENABLE_core:BOOL=ON \
     -D rompp_ENABLE_solvers:BOOL=ON \
     -D rompp_ENABLE_svd:BOOL=ON \
-    -D rompp_ENABLE_ode:BOOL=OFF \
-    -D rompp_ENABLE_apps:BOOL=OFF \
+    -D rompp_ENABLE_ode:BOOL=ON \
+    -D rompp_ENABLE_apps:BOOL=ON \
     -D rompp_ENABLE_optimization:BOOL=OFF \
     -D rompp_ENABLE_rom:BOOL=OFF \
     \
