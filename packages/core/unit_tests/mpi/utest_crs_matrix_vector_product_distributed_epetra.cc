2049221aa7249f4e4e43895de3c91e0fe5358d9d
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-09-08 16:58:43 +0200

    cleaning up mat vector tests in core unit testing

--- packages/core/unit_tests/mpi/utest_crs_matrix_vector_product_distributed_epetra.cc
+++ packages/core/unit_tests/mpi/utest_crs_matrix_vector_product_distributed_epetra.cc
@@ -1,4 +1,3 @@
-
 #include <gtest/gtest.h>
 #include "CORE_ALL"
 #include "Epetra_MpiComm.h"
@@ -13,15 +12,12 @@ public:
 
   core::Matrix<Epetra_CrsMatrix> * A_;
   core::Vector<Epetra_Vector> * b_;
-
   int nRowsSM_;
-  int nColsSM_;
   Epetra_Map * smMap_;
   int nRowsV_;
   Epetra_Map * vMap_;
   
-  virtual void SetUp()
-  {
+  virtual void SetUp() override{
     MPI_Comm_rank(MPI_COMM_WORLD, &rank_);
     Comm_ = new Epetra_MpiComm(MPI_COMM_WORLD);
     MyPID_ = Comm_->MyPID();
@@ -31,14 +27,13 @@ public:
     nRowsSM_ = 9;
     smMap_ = new Epetra_Map(nRowsSM_, 0, *Comm_);
     A_ = new core::Matrix<Epetra_CrsMatrix>(*smMap_, 5);
-
     nRowsV_ = 13;
     vMap_ = new Epetra_Map(nRowsV_, 0, *Comm_);
     vMap_->Print(std::cout);
     b_ = new core::Vector<Epetra_Vector>(*vMap_);
   }
   
-  virtual void TearDown(){
+  virtual void TearDown() override{
     delete Comm_;
     delete vMap_;
     delete smMap_;
@@ -47,12 +42,10 @@ public:
   }
 };
 
-TEST_F(core_matrix_vec_product_distributed_epetraFix, Test1)
-{
 
+TEST_F(core_matrix_vec_product_distributed_epetraFix, CRSMatTimesVector){
   //-----------
   // FILL A
-  //-----------
   {
     int myNR = smMap_->NumMyElements();
 
@@ -66,52 +59,45 @@ TEST_F(core_matrix_vec_product_distributed_epetraFix, Test1)
       	colind = {0, 4, 7, 11, 12};
       	A_->insertGlobalValues(it, 5, vals.data(), colind.data());
       }
-      if(it == 4){
+      if(it == 6){
       	vals = {1., 3., 6.};
-      	colind = {1, 3, 6};
+      	colind = {1, 4, 7};
       	A_->insertGlobalValues(it, 3, vals.data(), colind.data());
       }
 
     }
-    A_->fillingIsCompleted(*dmMap_, *smMap_);
+    A_->fillingIsCompleted(*vMap_, *smMap_);
     A_->data()->Print(std::cout);
   }
 
   //-----------
   // FILL b
   //-----------
-  {
-    if (MyPID_ == 0){
-      (*b_)[0] = 1.;
-      (*b_)[4] = 3.;
-    }
-    if (MyPID_ == 1){
-      (*b_)[2] = 1.;
-    }
-    if (MyPID_ == 2){
-      (*b_)[2] = 2.;
-      (*b_)[3] = 2.;
-    }
-    b_->data()->Print(std::cout);
+  if (MyPID_ == 0){
+    (*b_)[0] = 1.;
+    (*b_)[4] = 3.;
   }
+  if (MyPID_ == 1){
+    (*b_)[2] = 1.;
+  }
+  if (MyPID_ == 2){
+    (*b_)[2] = 2.;
+    (*b_)[3] = 2.;
+  }
+  b_->data()->Print(std::cout);
   
   //------------------
   // product: b = A b
   //------------------
-  // auto CC = core::matrixMatrixProduct(*A_, *B_);
-  // CC.data()->Print(std::cout);
+  auto c = core::mat_ops::product(*A_, *b_);
+  c.data()->Print(std::cout);
 
-  // assert( CC.globalRows() == 9);
-  // assert( CC.globalCols() == 5);
-  // static_assert( std::is_same<decltype(CC), core::Matrix<Epetra_MultiVector>>::value, "" );
-  // if (MyPID_ == 0){
-  //   EXPECT_DOUBLE_EQ( CC(0,0), 17. );
-  //   EXPECT_DOUBLE_EQ( CC(0,1), 0. );
-  //   EXPECT_DOUBLE_EQ( CC(0,2), 1. );
-  //   EXPECT_DOUBLE_EQ( CC(0,3), 2. );
-  //   EXPECT_DOUBLE_EQ( CC(0,4), 2. );
-  // }
-  // if (MyPID_ == 1)
-  //   EXPECT_DOUBLE_EQ( CC(1,2), 26. );
+  assert( c.globalSize() == 9);
+  static_assert( std::is_same<decltype(c),
+		 core::Vector<Epetra_Vector>>::value, "" );
+  if (MyPID_ == 0)
+    EXPECT_DOUBLE_EQ( c[0], 17. );
+   if (MyPID_ == 2)
+    EXPECT_DOUBLE_EQ( c[0], 15. );
   
 }
