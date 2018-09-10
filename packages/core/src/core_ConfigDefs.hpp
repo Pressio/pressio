54eafb61cc3609c34b30a6e3b022c7524f62036b
Francesco Rizzi <fr.francescorizzi@gmail.com>
2018-09-10 13:15:59 -0700

    removing commented lines in core_configDefs

--- packages/core/src/core_ConfigDefs.hpp
+++ packages/core/src/core_ConfigDefs.hpp
@@ -21,69 +21,9 @@ containers_shared_traits<void, void,
 
 template<typename T> 
 struct traits<const T> : traits<T> {};
-  
-
-// //---------------------------------------
-// // CRTP HELPER BASE CLASS
-// //---------------------------------------
-// template <typename T, typename enable = void>
-// struct CrtpBase;
-  
-// template <typename T,
-// 	  template<typename, typename...> class crtpType,
-// 	  typename ... Args>
-// struct CrtpBase< crtpType<T, Args...>>{
-//   T & underlying() {
-//     return static_cast<T&>(*this);
-//   }
-//   T const & underlying() const {
-//     return static_cast<T const&>(*this);
-//   }
-
-// private:
-//   CrtpBase() = default;
-//   ~CrtpBase() = default;
-//   friend crtpType<T, Args...>;
-// };//end class
-
-
-// template <typename T, int a, int b,
-// 	  template<typename, int, int> class crtpType>
-// struct CrtpBase< crtpType<T, a, b> >{
-//   T & underlying() {
-//     return static_cast<T&>(*this);
-//   }
-//   T const & underlying() const {
-//     return static_cast<T const&>(*this);
-//   }
-
-// private:
-//   CrtpBase() = default;
-//   ~CrtpBase() = default;
-//   friend crtpType<T, a, b>;
-// };//end class
 
   
-// template <typename T, int a,
-// 	  template<typename, int> class crtpType>
-// struct CrtpBase< crtpType<T, a> >{
-//   T & underlying() {
-//     return static_cast<T&>(*this);
-//   }
-//   T const & underlying() const {
-//     return static_cast<T const&>(*this);
-//   }
-
-// private:
-//   CrtpBase() = default;
-//   ~CrtpBase() = default;
-//   friend crtpType<T, a>;
-// };//end class
-
-  
-//------------------------
 } // end namespace details
-//------------------------
 
   
    
