c10aca83937e697442b029759f8e637fa0093cc0
Francesco Rizzi <fnrizzi@sandia.gov>
2018-12-06 14:06:07 -0700

    fixing some constants

--- packages/ode/src/implicit/ode_implicit_constants.hpp
+++ packages/ode/src/implicit/ode_implicit_constants.hpp
@@ -12,7 +12,6 @@ namespace rompp{ namespace ode{ namespace coeffs{
   static constexpr std::size_t bdf1_numAuxRHS_ = 0;
 
 
-
   // beside y_n, bdf2 needs y_n-1, y_n-2
   static constexpr std::size_t bdf2_numAuxStates_ = 2;
   // bdf2 needs f(y_n,...)
@@ -21,9 +20,9 @@ namespace rompp{ namespace ode{ namespace coeffs{
 
   template <typename scalar_t>
   struct bdf2{
-    static constexpr scalar_t c1_ = static_cast<scalar_t>(4)/3.;
-    static constexpr scalar_t c2_ = static_cast<scalar_t>(1)/3.;
-    static constexpr scalar_t c3_ = static_cast<scalar_t>(2)/3.;
+    static constexpr scalar_t c1_ = static_cast<scalar_t>(4)/3;
+    static constexpr scalar_t c2_ = static_cast<scalar_t>(1)/3;
+    static constexpr scalar_t c3_ = static_cast<scalar_t>(2)/3;
   };
 
 
