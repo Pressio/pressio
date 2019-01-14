53fc563a3d843190c1da89deca9578442225aedb
Francesco Rizzi <fnrizzi@sandia.gov>
2019-01-14 13:03:40 +0100

    core: printable base class for containers

---
+++ packages/core/src/shared_base/core_container_printable_base.hpp
@@ -0,0 +1,76 @@
+
+#ifndef CORE_SHARED_BASE_CONTAINER_PRINTABLE_BASE_HPP_
+#define CORE_SHARED_BASE_CONTAINER_PRINTABLE_BASE_HPP_
+
+#include "../core_ConfigDefs.hpp"
+
+namespace rompp{ namespace core{
+
+/*
+ * number of elements to prints defaults = -1 , indicating to print all
+ *
+ * the char c = 'd', 'f'
+ * 'd' = default, prints as they are, so for vectors prints vertically
+ * 'f' = flatten, for vectors prints horizontally,
+ *		  for matrices it falttens it
+ */
+
+template<typename derived_type, typename ord_t>
+class ContainerPrintable1DBase
+  : private core::details::CrtpBase<
+	ContainerPrintable1DBase<derived_type, ord_t>>{
+
+  using this_t = ContainerPrintable1DBase<derived_type, ord_t>;
+public:
+  template <typename stream_t>
+  void print(stream_t & os = std::cout,
+	     char c = 'd',
+	     ord_t numElementsToPrint = -1) const{
+    this->underlying().printImpl(os, c, numElementsToPrint);
+  }
+
+  void printStdCout(char c = 'd', ord_t numElementsToPrint = -1) const{
+    this->print(std::cout, c, numElementsToPrint);
+  }
+
+private:
+  friend derived_type;
+  friend core::details::CrtpBase<this_t>;
+  ContainerPrintable1DBase() = default;
+  ~ContainerPrintable1DBase() = default;
+};//end class
+
+
+
+template<typename derived_type, typename ord1_t, typename ord2_t = ord1_t>
+class ContainerPrintable2DBase
+  : private core::details::CrtpBase<
+  ContainerPrintable2DBase<derived_type, ord1_t, ord2_t>>{
+
+  using this_t = ContainerPrintable2DBase<derived_type, ord1_t, ord2_t>;
+public:
+  template <typename stream_t>
+  void print(stream_t & os = std::cout,
+	     char c = 'd',
+	     ord1_t niToPrint = -1,
+	     ord2_t njToPrint = -1) const{
+    this->underlying().printImpl(os, c, niToPrint, njToPrint);
+  }
+
+  void printStdCout(char c = 'd',
+		    ord1_t niToPrint = -1,
+		    ord2_t njToPrint = -1) const{
+    this->print(std::cout, c, niToPrint, njToPrint);
+  }
+
+private:
+  friend derived_type;
+  friend core::details::CrtpBase<this_t>;
+
+  ContainerPrintable2DBase() = default;
+  ~ContainerPrintable2DBase() = default;
+
+};//end class
+
+}}//end namespace rompp::core
+#endif
