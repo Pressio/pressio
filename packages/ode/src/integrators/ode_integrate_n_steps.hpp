2bd610872ade3324aad384693c2c4a6d5275f622
Francesco Rizzi <fnrizzi@sandia.gov>
2019-01-14 22:48:01 +0100

    overload for collector and guesser

--- packages/ode/src/integrators/ode_integrate_n_steps.hpp
+++ packages/ode/src/integrators/ode_integrate_n_steps.hpp
@@ -132,6 +132,36 @@ void integrateNSteps(stepper_type   & stepper,
 }
 
 
+
+template<typename stepper_type,   typename state_type,
+	 typename time_type,	  typename integral_type,
+	 typename collector_type, typename solver_type,
+	 typename guess_callback_t,
+	 typename std::enable_if<
+	   ode::meta::is_legitimate_collector<
+	     collector_type, integral_type,
+	     time_type, state_type>::value &&
+	   std::is_integral<integral_type>::value &&
+	   details::traits<stepper_type>::is_implicit
+	   >::type * = nullptr>
+void integrateNSteps(stepper_type   & stepper,
+		     state_type	    & yIn,
+		     time_type	      start_time,
+		     time_type	      dt,
+		     integral_type    num_steps,
+		     collector_type & collector,
+		     solver_type    & solver,
+		     guess_callback_t && guessCb){
+  using one_step_impl_t = impl::DoStepMixin<solver_type, guess_callback_t>;
+  using advancer_impl_t = impl::AdvancerMixin<collector_type,
+					      one_step_impl_t>;
+
+  advancer_impl_t advancer;
+  advancer(num_steps, start_time, dt, yIn, collector, stepper,
+	   collector, solver, std::forward<guess_callback_t>(guessCb));
+}
+
+
 }}//end namespace rompp::ode
 #endif
 
