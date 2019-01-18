ff7e6d0ff4f0b58ee481dccd0e4f9aae160b6802
Francesco Rizzi <fnrizzi@sandia.gov>
2019-01-18 15:17:02 +0100

    solvers: cleanup utest; putting test problems into separate dir

--- packages/solvers/unit_tests/serial/non_linear_least_squares/gn_qr_exp_data_fit_n2.cc
+++ packages/solvers/unit_tests/serial/non_linear_least_squares/gn_qr_exp_data_fit_n2.cc
@@ -1,67 +1,28 @@
 
 #include <gtest/gtest.h>
-#include "CORE_ALL"
 #include "SOLVERS_NONLINEAR"
 #include "QR_BASIC"
+#include "./problems/solvers_utest_serial_expon_data_fit_n2.hpp"
 
-struct NonLinearLeastSquareSystem {
-  //using jacobian_w_t = rompp::core::Matrix<Eigen::SparseMatrix<double>>;
-  using jacobian_w_t = rompp::core::Matrix<Eigen::MatrixXd>;
-  using state_w_t = rompp::core::Vector<Eigen::VectorXd>;
-  using state_type = state_w_t;
-  using residual_type = state_type;
-  using jacobian_type =  jacobian_w_t;
-  static constexpr int n = 8;
-  const double times_[n] = {1.,2.,3.,4.,
-			   5.,6.,7.,8};
-  const double y_[n] = {3.29, 4.27, 5.3, 7.1,
-		       10.1, 9.8, 16.1, 20.2};
-
-  void residual(const state_type& x, residual_type & res) const {
-    for (auto i = 0; i < n; i++) {
-      res[i] = x[0] * exp(x[1]*times_[i]) - y_[i];
-    }
-  }
-
-  residual_type residual(const state_type& x) const {
-    residual_type res(n);
-    this->residual(x, res);
-    return res;
-  }
-
-  void jacobian(const state_type & x, jacobian_type & jac) const {
-    for (int i = 0; i < n; i++) {
-      double expval = exp(x[1] * times_[i]);
-      (*jac.data())(i,0) = expval; //->coeffRef(i, 0) = expval;
-      (*jac.data())(i,1) = x[0]*times_[i]*expval;
-      //jac.data()->coeffRef(i,0) = expval;
-      //jac.data()->coeffRef(i, 1) = x[0]*times_[i]*expval;
-    }
-  }
-
-  jacobian_type jacobian(const state_type& x) const {
-    jacobian_type jac(n, 2);
-    this->jacobian(x, jac);
-    return jac;
-  }
-};
-
-
-TEST(solvers_nonlinear_least_squares, gaussNewtonQR){
+TEST(solvers_nonlinear_least_squares, gn_qr_exp_data_fit_n2){
   using namespace rompp;
-  using state_w_t = core::Vector<Eigen::VectorXd>;
+
+  using problem_t   = solvers::test::ExpDataFitN2;
+
+  using state_w_t = typename problem_t::state_type;
   using sc_t	  = double;
-  using mat_type  = typename NonLinearLeastSquareSystem::jacobian_w_t;
-  NonLinearLeastSquareSystem problem;
+  using mat_type  = typename problem_t::jacobian_type;
+  problem_t problem;
+
+  state_w_t x(2); x[0] = 2.0; x[1] = 0.25;
 
   // define type of QR and GaussNewton solver
   using qr_algo = qr::Householder;
   using qr_type = qr::QRSolver<mat_type, qr_algo>;
   solvers::iterative::GaussNewtonQR<sc_t, qr_type> solver;
   solver.setTolerance(1e-8);
-
-  state_w_t x(2); x[0] = 2.0; x[1] = 0.25;
   solver.solve(problem, x);
+
   std::cout << std::setprecision(14) << *x.data() << std::endl;
   EXPECT_NEAR( x(0), 2.4173449278229, 1e-9 );
   EXPECT_NEAR( x(1), 0.26464986197941, 1e-9 );
@@ -73,12 +34,14 @@ TEST(solvers_nonlinear_least_squares, gaussNewtonQR){
 }
 
 
-TEST(solvers_nonlinear_least_squares, gaussNewtonQRDoOnly2Steps){
+TEST(solvers_nonlinear_least_squares, gn_qr_only_2_steps_exp_data_fit_n2){
   using namespace rompp;
-  using state_w_t = core::Vector<Eigen::VectorXd>;
+
+  using problem_t   = solvers::test::ExpDataFitN2;
+  using state_w_t = typename problem_t::state_type;
   using sc_t	  = double;
-  using mat_type  = typename NonLinearLeastSquareSystem::jacobian_w_t;
-  NonLinearLeastSquareSystem problem;
+  using mat_type  = typename problem_t::jacobian_type;
+  problem_t problem;
 
   // define type of QR and GaussNewton solver
   using qr_algo = qr::Householder;
@@ -99,14 +62,17 @@ TEST(solvers_nonlinear_least_squares, gaussNewtonQRDoOnly2Steps){
 }
 
 
-TEST(solvers_nonlinear_least_squares, gaussNewtonQRPassTypes){
-  using namespace rompp;
-  using problem_t = NonLinearLeastSquareSystem;
-  using state_w_t = typename problem_t::state_w_t;
-  using sc_t	  = double;
-  using mat_t	  = typename problem_t::jacobian_w_t;
+TEST(solvers_nonlinear_least_squares,
+     gn_qr_only_pass_sys_type_exp_data_fit_n2){
 
+  using namespace rompp;
+
+  using problem_t   = solvers::test::ExpDataFitN2;
+  using state_w_t = typename problem_t::state_type;
+  using sc_t    = double;
+  using mat_t   = typename problem_t::jacobian_type;
   problem_t problem;
+
   state_w_t x(2); x[0] = 2.0; x[1] = 0.25;
 
   // define type of QR solver and GaussNewton solver
