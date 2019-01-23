64982deb94ca12c289784845a5b4c314c07bd27a
Francesco Rizzi <fnrizzi@sandia.gov>
2019-01-23 14:09:42 +0100

    rom: commenting out galerkin for now

--- packages/rom/src/galerkin/rom_galerkin_explicit_residual_policy.hpp
+++ packages/rom/src/galerkin/rom_galerkin_explicit_residual_policy.hpp
@@ -6,98 +6,98 @@
 #include "../../../CORE_ALL"
 #include "../../../ode/src/explicit/policies/ode_explicit_residual_policy_base.hpp"
 //#include "../rom_incremental_solution_base.hpp"
-#include "../rom_data_base.hpp"
+// #include "../rom_data_base.hpp"
 
 namespace rompp{ namespace rom{
 
-template<typename state_type,
-	 typename space_res_type,
-	 typename phi_op_type,
-	 typename A_type = phi_op_type>
-class RomGalerkinExplicitResidualPolicy
-  : public ode::policy::ExplicitResidualPolicyBase<
-                RomGalerkinExplicitResidualPolicy<
-		  state_type, space_res_type, phi_op_type, A_type>>,
-    protected RomStateData<state_type, phi_op_type, 0>,
-    protected RomRHSData<space_res_type, 1>{
+// template<typename state_type,
+// 	 typename space_res_type,
+// 	 typename phi_op_type,
+// 	 typename A_type = phi_op_type>
+// class RomGalerkinExplicitResidualPolicy
+//   : public ode::policy::ExplicitResidualPolicyBase<
+//                 RomGalerkinExplicitResidualPolicy<
+// 		  state_type, space_res_type, phi_op_type, A_type>>,
+//     protected RomStateData<state_type, phi_op_type, 0>,
+//     protected RomRHSData<space_res_type, 1>{
 
-  using this_t = RomGalerkinExplicitResidualPolicy<state_type,
-						   space_res_type,
-						   phi_op_type,
-						   A_type>;
+//   using this_t = RomGalerkinExplicitResidualPolicy<state_type,
+// 						   space_res_type,
+// 						   phi_op_type,
+// 						   A_type>;
 
-  using base_pol_t = ode::policy::ExplicitResidualPolicyBase<this_t>;
+//   using base_pol_t = ode::policy::ExplicitResidualPolicyBase<this_t>;
 
-  using base_state_data_t = ::rompp::rom::RomStateData<state_type, phi_op_type, 0>;
+//   using base_state_data_t = ::rompp::rom::RomStateData<state_type, phi_op_type, 0>;
 
-  using base_rhs_data_t = ::rompp::rom::RomRHSData<space_res_type, 1>;
+//   using base_rhs_data_t = ::rompp::rom::RomRHSData<space_res_type, 1>;
 
 
-private:
-  A_type * A_ = nullptr;
+// private:
+//   A_type * A_ = nullptr;
 
-  using base_state_data_t::y0FOM_;
-  using base_state_data_t::yFOM_;
-  using base_state_data_t::phi_;
-  using base_rhs_data_t::appRHS_;
+//   using base_state_data_t::y0FOM_;
+//   using base_state_data_t::yFOM_;
+//   using base_state_data_t::phi_;
+//   using base_rhs_data_t::appRHS_;
 
-public:
+// public:
 
-  template <typename T=A_type,
-   core::meta::enable_if_t<
-     std::is_same<T,phi_op_type>::value
-     > * = nullptr>
-  RomGalerkinExplicitResidualPolicy(const state_type & y0fom,
-				    const space_res_type & r0fom,
-				    phi_op_type & phiOp)
-    : base_state_data_t(y0fom, phiOp),
-      base_rhs_data_t(r0fom),
-      A_(&phiOp){}
+//   template <typename T=A_type,
+//    core::meta::enable_if_t<
+//      std::is_same<T,phi_op_type>::value
+//      > * = nullptr>
+//   RomGalerkinExplicitResidualPolicy(const state_type & y0fom,
+// 				    const space_res_type & r0fom,
+// 				    phi_op_type & phiOp)
+//     : base_state_data_t(y0fom, phiOp),
+//       base_rhs_data_t(r0fom),
+//       A_(&phiOp){}
 
-  RomGalerkinExplicitResidualPolicy() = delete;
+//   RomGalerkinExplicitResidualPolicy() = delete;
 
-  ~RomGalerkinExplicitResidualPolicy() = default;
+//   ~RomGalerkinExplicitResidualPolicy() = default;
 
-public:
+// public:
 
-  template <typename ode_state_t,
-	    typename ode_res_t,
-	    typename app_type,
-	    typename scalar_type>
-  void operator()(const ode_state_t & odeY,
-		  ode_res_t & odeR,
-		  const app_type & app,
-		  scalar_type t) const {
+//   template <typename ode_state_t,
+// 	    typename ode_res_t,
+// 	    typename app_type,
+// 	    typename scalar_type>
+//   void operator()(const ode_state_t & odeY,
+// 		  ode_res_t & odeR,
+// 		  const app_type & app,
+// 		  scalar_type t) const {
 
-    /*- odeY: has type = ode_state_t, NOT for sure = state_type
-     *  - odeR: has type = ode_res_t, NOT for sure = space_res_type
+//     /*- odeY: has type = ode_state_t, NOT for sure = state_type
+//      *  - odeR: has type = ode_res_t, NOT for sure = space_res_type
 
-     * Typically ode_types are different!
-     *
-     * Because the way we compute ROM stuff does not have to
-     * be the same as the types used by the application.
-     * It can be, but not necessarily.
-     */
+//      * Typically ode_types are different!
+//      *
+//      * Because the way we compute ROM stuff does not have to
+//      * be the same as the types used by the application.
+//      * It can be, but not necessarily.
+//      */
 
-    // odeY is the REDUCED state, we need to reconstruct FOM state
-    phi_->apply(odeY, yFOM_);
+//     // odeY is the REDUCED state, we need to reconstruct FOM state
+//     phi_->apply(odeY, yFOM_);
 
-    // since we are advancing the Incremental Solution,
-    // to compute the app residual we need to add the
-    // FOM initial condition to get full state
-    yFOM_ += *y0FOM_;
+//     // since we are advancing the Incremental Solution,
+//     // to compute the app residual we need to add the
+//     // FOM initial condition to get full state
+//     yFOM_ += *y0FOM_;
 
-    /// query the application for the SPACE residual
-    app.residual(*yFOM_.data(), *appRHS_[0].data(), t);
+//     /// query the application for the SPACE residual
+//     app.residual(*yFOM_.data(), *appRHS_[0].data(), t);
 
-    /// apply weighting
-    A_->applyTranspose(appRHS_[0], odeR);
-  }
+//     /// apply weighting
+//     A_->applyTranspose(appRHS_[0], odeR);
+//   }
 
-private:
-  friend base_pol_t;
+// private:
+//   friend base_pol_t;
 
-};//end class
+// };//end class
 
 }}//end namespace rompp::rom
 #endif
--- packages/rom/src/galerkin/rom_gelerkin_implicit_euler_residual_policy.hpp
+++ packages/rom/src/galerkin/rom_gelerkin_implicit_euler_residual_policy.hpp
@@ -6,136 +6,136 @@
 #include "../../../CORE_ALL"
 #include "../../../ode/src/implicit/ode_residual_impl.hpp"
 #include "../../../ode/src/implicit/policies/base/ode_implicit_residual_policy_base.hpp"
-#include "../rom_incremental_solution_base.hpp"
+// #include "../rom_incremental_solution_base.hpp"
 
 namespace rompp{ namespace rom{ 
     
-template<typename app_state_w_type,
-	 typename app_res_w_type,
-	 typename phi_op_type,
-	 typename ode_state_w_type,
-	 typename ode_res_w_type,
-	 typename A_type>
-class RomGalerkinImplicitResidualPolicy<ode::ImplicitEnum::Euler,
-					app_state_w_type, app_res_w_type,
-					phi_op_type, ode_state_w_type,
-					ode_res_w_type, A_type>
-  : // inherits from implicit residual policy base
-    public ode::policy::ImplicitResidualPolicyBase<
-                RomGalerkinImplicitResidualPolicy<
-		  ode::ImplicitEnum::Euler,
-		  app_state_w_type, app_res_w_type,
-		  phi_op_type, ode_state_w_type,
-		  ode_res_w_type, A_type>>,
-    // inherits from incremental solution
-    private IncrementalSolutionBase<
-                RomGalerkinImplicitResidualPolicy<
-		  ::rompp::ode::ImplicitEnum::Euler,
-		  app_state_w_type, app_res_w_type,
-		  phi_op_type, ode_state_w_type,
-		  ode_res_w_type, A_type>, app_state_w_type>
-{
+// template<typename app_state_w_type,
+// 	 typename app_res_w_type,
+// 	 typename phi_op_type,
+// 	 typename ode_state_w_type,
+// 	 typename ode_res_w_type,
+// 	 typename A_type>
+// class RomGalerkinImplicitResidualPolicy<ode::ImplicitEnum::Euler,
+// 					app_state_w_type, app_res_w_type,
+// 					phi_op_type, ode_state_w_type,
+// 					ode_res_w_type, A_type>
+//   : // inherits from implicit residual policy base
+//     public ode::policy::ImplicitResidualPolicyBase<
+//                 RomGalerkinImplicitResidualPolicy<
+// 		  ode::ImplicitEnum::Euler,
+// 		  app_state_w_type, app_res_w_type,
+// 		  phi_op_type, ode_state_w_type,
+// 		  ode_res_w_type, A_type>>,
+//     // inherits from incremental solution
+//     private IncrementalSolutionBase<
+//                 RomGalerkinImplicitResidualPolicy<
+// 		  ::rompp::ode::ImplicitEnum::Euler,
+// 		  app_state_w_type, app_res_w_type,
+// 		  phi_op_type, ode_state_w_type,
+// 		  ode_res_w_type, A_type>, app_state_w_type>
+// {
   
-  using this_t = RomGalerkinImplicitResidualPolicy<
-    ::rompp::ode::ImplicitEnum::Euler,
-    app_state_w_type, app_res_w_type, phi_op_type,
-    ode_state_w_type, ode_res_w_type, A_type>;
+//   using this_t = RomGalerkinImplicitResidualPolicy<
+//     ::rompp::ode::ImplicitEnum::Euler,
+//     app_state_w_type, app_res_w_type, phi_op_type,
+//     ode_state_w_type, ode_res_w_type, A_type>;
 
-  using base_pol_t = ode::policy::ImplicitResidualPolicyBase<this_t>;
-  using base_incr_sol_t = IncrementalSolutionBase<this_t, app_state_w_type>;
-  using scalar_type = typename core::details::traits<app_state_w_type>::scalar_t;
+//   using base_pol_t = ode::policy::ImplicitResidualPolicyBase<this_t>;
+//   using base_incr_sol_t = IncrementalSolutionBase<this_t, app_state_w_type>;
+//   using scalar_type = typename core::details::traits<app_state_w_type>::scalar_t;
   
- private:
-  mutable app_res_w_type appRHS_;
-  mutable app_state_w_type yFOMnm1_;
-  phi_op_type * phi_ = nullptr;
-  A_type * A_ = nullptr;  
-  using base_incr_sol_t::y0FOM_;
-  using base_incr_sol_t::yFOM_;
+//  private:
+//   mutable app_res_w_type appRHS_;
+//   mutable app_state_w_type yFOMnm1_;
+//   phi_op_type * phi_ = nullptr;
+//   A_type * A_ = nullptr;  
+//   using base_incr_sol_t::y0FOM_;
+//   using base_incr_sol_t::yFOM_;
  
 
-public:
+// public:
 
-  // default constructor deleted
-  RomGalerkinImplicitResidualPolicy() = delete;
-  // default destructor
-  ~RomGalerkinImplicitResidualPolicy() = default;
+//   // default constructor deleted
+//   RomGalerkinImplicitResidualPolicy() = delete;
+//   // default destructor
+//   ~RomGalerkinImplicitResidualPolicy() = default;
 
-  template <typename T=A_type,
-   core::meta::enable_if_t<std::is_same<T,phi_op_type>::value> * = nullptr>
-  RomGalerkinImplicitResidualPolicy(const app_state_w_type & y0fom,
-				    const app_res_w_type & r0fom,
-				    phi_op_type & phiOp)
-    : base_incr_sol_t(y0fom), appRHS_(r0fom),
-      yFOMnm1_(y0fom), phi_(&phiOp), A_(&phiOp){}
+//   template <typename T=A_type,
+//    core::meta::enable_if_t<std::is_same<T,phi_op_type>::value> * = nullptr>
+//   RomGalerkinImplicitResidualPolicy(const app_state_w_type & y0fom,
+// 				    const app_res_w_type & r0fom,
+// 				    phi_op_type & phiOp)
+//     : base_incr_sol_t(y0fom), appRHS_(r0fom),
+//       yFOMnm1_(y0fom), phi_(&phiOp), A_(&phiOp){}
 
   
-public:
+// public:
   
-  //---------------------------------------------------------------------------
-  // compute: R( hat(y)_n) = hat(y)_n - hat(y)_n-1 - dt * A^T * f(phi hat(y))  
-  //---------------------------------------------------------------------------
-  template <typename app_t>
-  ode_res_w_type operator()(const ode_state_w_type & odeY,
-			    const std::array<ode_state_w_type, 1> & prevYs,
-			    const app_t & app,
-			    scalar_type t,
-			    scalar_type dt) const{
+//   //---------------------------------------------------------------------------
+//   // compute: R( hat(y)_n) = hat(y)_n - hat(y)_n-1 - dt * A^T * f(phi hat(y))  
+//   //---------------------------------------------------------------------------
+//   template <typename app_t>
+//   ode_res_w_type operator()(const ode_state_w_type & odeY,
+// 			    const std::array<ode_state_w_type, 1> & prevYs,
+// 			    const app_t & app,
+// 			    scalar_type t,
+// 			    scalar_type dt) const{
     
-    ode_res_w_type modRHS(odeY);//this is just to construct it 
-    modRHS.setZero();
-    // (*this)(odeY, modRHS, prevYs, app, t, dt);
+//     ode_res_w_type modRHS(odeY);//this is just to construct it 
+//     modRHS.setZero();
+//     // (*this)(odeY, modRHS, prevYs, app, t, dt);
 
 
-   // // odeY is the REDUCED state, we need to reconstruct FOM state
-   // reconstructFOMState(odeY, prevYs[0]);
-   // /// query the application for the SPACE residual 
-   // app.residual(*yFOM_.data(), *appRHS_.data(), t);
-   // /// apply operator
-   // A_->applyTranspose(appRHS_, modRHS);    
-   // /// do time discrete residual
-   // ode::impl::implicit_euler_time_discrete_residual(odeY, prevYs[0], modRHS, dt);
-    return modRHS;
-  }  
-  //----------------------------------------------------------------
+//    // // odeY is the REDUCED state, we need to reconstruct FOM state
+//    // reconstructFOMState(odeY, prevYs[0]);
+//    // /// query the application for the SPACE residual 
+//    // app.residual(*yFOM_.data(), *appRHS_.data(), t);
+//    // /// apply operator
+//    // A_->applyTranspose(appRHS_, modRHS);    
+//    // /// do time discrete residual
+//    // ode::impl::implicit_euler_time_discrete_residual(odeY, prevYs[0], modRHS, dt);
+//     return modRHS;
+//   }  
+//   //----------------------------------------------------------------
   
-  template <typename app_t>
-  void operator()(const ode_state_w_type & odeY,
-  		  ode_res_w_type & odeR,
-  		  const std::array<ode_state_w_type, 1> & prevYs,
-  		  const app_t & app,
-  		  scalar_type t,
-  		  scalar_type dt) const{
+//   template <typename app_t>
+//   void operator()(const ode_state_w_type & odeY,
+//   		  ode_res_w_type & odeR,
+//   		  const std::array<ode_state_w_type, 1> & prevYs,
+//   		  const app_t & app,
+//   		  scalar_type t,
+//   		  scalar_type dt) const{
 
-    // // odeY is the REDUCED state, we need to reconstruct FOM state
-    // reconstructFOMState(odeY, prevYs[0]);
+//     // // odeY is the REDUCED state, we need to reconstruct FOM state
+//     // reconstructFOMState(odeY, prevYs[0]);
 
-    // /// query the application for the SPACE residual 
-    // app.residual(*yFOM_.data(), *appRHS_.data(), t);
+//     // /// query the application for the SPACE residual 
+//     // app.residual(*yFOM_.data(), *appRHS_.data(), t);
 
-    // /// apply operator
-    // A_->applyTranspose(appRHS_, odeR);
+//     // /// apply operator
+//     // A_->applyTranspose(appRHS_, odeR);
 
-    // /// do time discrete residual
-    // ode::impl::implicit_euler_time_discrete_residual(odeY, prevYs[0], odeR, dt);
-  }
-  //----------------------------------------------------------------
+//     // /// do time discrete residual
+//     // ode::impl::implicit_euler_time_discrete_residual(odeY, prevYs[0], odeR, dt);
+//   }
+//   //----------------------------------------------------------------
 
- private:
-  void reconstructFOMState(const ode_state_w_type & odeY,
-			   const ode_state_w_type & odeYm1)const {
-    // odeY is the REDUCED state, reconstruct FOM state and add FOM(t=0)
-    // because we are advancing the Incremental Solution
-    phi_->apply(odeY, yFOM_);
-    yFOM_ += (*y0FOM_);
-  }
-  //----------------------------------------------------------------
+//  private:
+//   void reconstructFOMState(const ode_state_w_type & odeY,
+// 			   const ode_state_w_type & odeYm1)const {
+//     // odeY is the REDUCED state, reconstruct FOM state and add FOM(t=0)
+//     // because we are advancing the Incremental Solution
+//     phi_->apply(odeY, yFOM_);
+//     yFOM_ += (*y0FOM_);
+//   }
+//   //----------------------------------------------------------------
   
-private:
-  friend base_pol_t;
-  friend base_incr_sol_t;
+// private:
+//   friend base_pol_t;
+//   friend base_incr_sol_t;
 
-};//end class
+// };//end class
   
 }}//end namespace rompp::rom
 #endif 
