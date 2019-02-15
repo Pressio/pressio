7039d4dc023001a9e4ff1e570e7df538eda7e4ec
Francesco Rizzi <fnrizzi@sandia.gov>
2019-02-15 14:26:53 +0100

    core: adding self dot for multivectors to smartly compute A^T*A

---
+++ packages/core/src/containers_ops/dot_self/core_multi_vector_dot_self.hpp
@@ -0,0 +1,8 @@
+
+#ifndef CORE_MULTI_VECTOR_DOT_SELF_HPP__
+#define CORE_MULTI_VECTOR_DOT_SELF_HPP_
+
+#include "mvec/core_epetra_multi_vector_dot_self.hpp"
+#include "mvec/core_tpetra_multi_vector_dot_self.hpp"
+
+#endif
