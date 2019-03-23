9032836345fb3d75b160444041cbcd9fff4e587a
Francesco Rizzi <fnrizzi@sandia.gov>
2019-03-23 15:37:27 +0100

    apps: reordering of fom tests

---
+++ packages/apps/test/burgers1d/fom/eigen_implicit_euler_observer/main.cc
@@ -0,0 +1,97 @@
+
+#include "CORE_ALL"
+#include "ODE_ALL"
+#include "SOLVERS_NONLINEAR"
+#include "APPS_BURGERS1D"
+#include "../fom_gold_states.hpp"
+
+constexpr double eps = 1e-12;
+
+template <typename state_t>
+struct observer{
+  using matrix_t = Eigen::MatrixXd;
+
+  size_t state_size_ {};
+  matrix_t A_;
+  size_t count_ {};
+  state_t y0_;
+  state_t yIncr_;
+
+  observer(int N, int state_size, const state_t & y0)
+    : state_size_(state_size),
+      A_(state_size, N+1), //+1 to store also init cond
+      y0_(y0),
+      yIncr_(state_size){}
+
+  void operator()(size_t step,
+  		  double t,
+  		  const state_t & y){
+    yIncr_ = y - y0_;
+    this->storeInColumn(yIncr_, count_);
+    count_++;
+  }
+
+  void storeInColumn(const state_t & y, int j){
+    for (auto i=0; i<y.size(); i++)
+      A_(i,j) = y(i);
+  }
+  size_t getCount() const{ return count_;}
+  void printAll() const{ std::cout << A_ << std::endl; }
+};
+
+
+template <typename T>
+void checkSol(const T & y, const std::vector<double> & trueS){
+  for (size_t i=0; i< trueS.size(); i++)
+    assert(std::abs(y[i] - trueS[i]) < eps);
+}
+
+int main(int argc, char *argv[]){
+  using app_t		= rompp::apps::Burgers1dEigen;
+  using scalar_t	= typename app_t::scalar_type;
+  using app_state_t	= typename app_t::state_type;
+  using app_rhs_t	= typename app_t::residual_type;
+  using app_jacob_t	= typename app_t::jacobian_type;
+
+  //-------------------------------
+  // create app object
+  constexpr int Ncell = 20;
+  Eigen::Vector3d mu(5.0, 0.02, 0.02);
+  app_t appObj(mu, Ncell);
+  appObj.setup();
+  auto & y0n = appObj.getInitialState();
+
+  // types for ode
+  using ode_state_t = rompp::core::Vector<app_state_t>;
+  using ode_res_t   = rompp::core::Vector<app_rhs_t>;
+  using ode_jac_t   = rompp::core::Matrix<app_jacob_t>;
+
+  ode_state_t y(y0n);
+  using stepper_t = rompp::ode::ImplicitStepper<
+    rompp::ode::ImplicitEnum::Euler,
+    ode_state_t, ode_res_t, ode_jac_t, app_t>;
+  stepper_t stepperObj(appObj, y);
+
+  // define solver
+  using lin_solver_t = rompp::solvers::EigenIterative<
+    rompp::solvers::linear::Bicgstab, ode_jac_t>;
+  rompp::solvers::NewtonRaphson<scalar_t, lin_solver_t> solverO;
+  solverO.setTolerance(1e-13);
+  solverO.setMaxIterations(200);
+
+  // integrate in time
+  scalar_t fint = 0.10;
+  scalar_t dt = 0.01;
+  auto Nsteps = static_cast<unsigned int>(fint/dt);
+
+  // define observer
+  observer<ode_state_t> Obs(Nsteps, Ncell, y);
+
+  rompp::ode::integrateNSteps(stepperObj, y, 0.0, dt, Nsteps, Obs, solverO);
+  Obs.printAll();
+
+  checkSol(y, rompp::apps::test::Burg1DtrueImpEulerN20t010);
+  std::cout << std::setprecision(14) << *y.data();
+
+  return 0;
+}
---
+++ packages/apps/test/burgers1d/fom/tpetra_explicit_euler/main.cc
@@ -0,0 +1,87 @@
+
+#include "CORE_ALL"
+#include "ODE_ALL"
+#include "APPS_BURGERS1D"
+#include "../fom_gold_states.hpp"
+
+constexpr double eps = 1e-12;
+
+template <typename T>
+void checkSol(int rank, const T & y,
+	      const std::vector<double> & trueS){
+  auto y_v = y.data()->getData();
+  if (rank==0){
+    assert(std::abs(y_v[0] - trueS[0]) < eps);
+    assert(std::abs(y_v[1] - trueS[1]) < eps);
+    assert(std::abs(y_v[2] - trueS[2]) < eps);
+    assert(std::abs(y_v[3] - trueS[3]) < eps);
+    assert(std::abs(y_v[4] - trueS[4]) < eps);}
+
+  if (rank==1){
+    assert(std::abs(y_v[0] - trueS[5]) < eps);
+    assert(std::abs(y_v[1] - trueS[6]) < eps);
+    assert(std::abs(y_v[2] - trueS[7]) < eps);
+    assert(std::abs(y_v[3] - trueS[8]) < eps);
+    assert(std::abs(y_v[4] - trueS[9]) < eps);}
+
+  if (rank==2){
+    assert(std::abs(y_v[0] - trueS[10]) < eps);
+    assert(std::abs(y_v[1] - trueS[11]) < eps);
+    assert(std::abs(y_v[2] - trueS[12]) < eps);
+    assert(std::abs(y_v[3] - trueS[13]) < eps);
+    assert(std::abs(y_v[4] - trueS[14]) < eps);}
+
+  if (rank==3){
+    assert(std::abs(y_v[0] - trueS[15]) < eps);
+    assert(std::abs(y_v[1] - trueS[16]) < eps);
+    assert(std::abs(y_v[2] - trueS[17]) < eps);
+    assert(std::abs(y_v[3] - trueS[18]) < eps);
+    assert(std::abs(y_v[4] - trueS[19]) < eps);}
+}
+
+int main(int argc, char *argv[]){
+  using namespace rompp;
+  using app_t		= rompp::apps::Burgers1dTpetra;
+  using scalar_t	= typename app_t::scalar_type;
+  using app_state_t	= typename app_t::state_type;
+  using app_residual_t	= typename app_t::residual_type;
+
+  using tcomm_t		= Teuchos::MpiComm<int>;
+  using rcpcomm_t	= Teuchos::RCP<const tcomm_t>;
+
+  // MPI init
+  MPI_Init(&argc,&argv);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  rcpcomm_t Comm = Teuchos::rcp (new tcomm_t(MPI_COMM_WORLD));
+  assert( Comm->getSize() == 4);
+
+  // scope guard needed for tpetra
+  Tpetra::ScopeGuard tpetraScope (&argc, &argv);
+  {
+    std::vector<double> mu({5.0, 0.02, 0.02});
+    app_t appobj(mu, 20, Comm);
+    appobj.setup();
+    auto & y0n = appobj.getInitialState();
+    auto r0n = appobj.residual(y0n, static_cast<scalar_t>(0));
+
+    using ode_state_t = core::Vector<app_state_t>;
+    using ode_res_t   = core::Vector<app_residual_t>;
+    ode_state_t y(y0n);
+    ode_res_t r(r0n);
+
+    using stepper_t = ode::ExplicitStepper<
+      ode::ExplicitEnum::Euler, ode_state_t, app_t, ode_res_t>;
+    stepper_t stepperObj(appobj, y, r);
+
+    // integrate in time
+    scalar_t fint = 35;
+    scalar_t dt = 0.01;
+    auto Nsteps = static_cast<unsigned int>(fint/dt);
+    ode::integrateNSteps(stepperObj, y, 0.0, dt, Nsteps);
+    checkSol(rank, y, rompp::apps::test::Burg1DtrueExpEulerN20t35);
+  }
+
+  MPI_Finalize();
+  return 0;
+}
