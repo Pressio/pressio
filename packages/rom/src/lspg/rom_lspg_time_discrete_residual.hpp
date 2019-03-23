6aaf77162f1769e28a59dffa69397dcaa88e87df
Francesco Rizzi <fnrizzi@sandia.gov>
2019-03-23 17:46:09 +0100

    rom: fix bdf2 part of time discrete residual in rom lspg
    Reason it was working for epetra but not for tpetra was that
    tpetra had a wrong sign (- instead of +) in from of second coefficient.
    Now fixed and also added test for tpetra as well.

--- packages/rom/src/lspg/rom_lspg_time_discrete_residual.hpp
+++ packages/rom/src/lspg/rom_lspg_time_discrete_residual.hpp
@@ -146,7 +146,7 @@ struct time_discrete_single_entry_tpetra<::rompp::ode::ImplicitEnum::BDF2>{
     //   - bdf2<scalar_type>::c3_*dt*R[i];
     R = (yn.data()->getData())[lid]
       - bdf2<T>::c1_*(ynm[1].data()->getData())[lid]
-      - bdf2<T>::c2_*(ynm[0].data()->getData())[lid]
+      + bdf2<T>::c2_*(ynm[0].data()->getData())[lid]
       - bdf2<T>::c3_*dt*R;
   }
 };
@@ -236,67 +236,11 @@ void time_discrete_residual(const state_type & yn,
 
   // // On input: R contains the application RHS, i.e. if
   // // dudt = f(x,u,...), R contains f(...)
-
   R = yn
     - bdf2<scalar_type>::c1_*ynm[1]
-    - bdf2<scalar_type>::c2_*ynm[0]
+    + bdf2<scalar_type>::c2_*ynm[0]
     - bdf2<scalar_type>::c3_*dt*R;
 }
 
-
 }}}//end namespace rompp::rom::impl
 #endif
-
-
-
-
-
-
-
-
-
-// #ifdef HAVE_TRILINOS
-// template<::rompp::ode::ImplicitEnum odeMethod,
-// 	  int numStates,
-// 	  typename state_type,
-// 	  typename scalar_type,
-// 	  core::meta::enable_if_t<
-//   (odeMethod == ::rompp::ode::ImplicitEnum::Euler)
-//   and ::rompp::core::meta::is_vector_wrapper_tpetra<state_type>::value
-//   > * = nullptr
-// 	  >
-// void time_discrete_residual(const state_type & yn,
-// 			    const std::array<state_type,numStates> & ynm,
-// 			    state_type & R,
-// 			    scalar_type dt){
-//   // On input: R should contain the application RHS, i.e. if
-//   //           dudt = f(x,u,...), R contains f(...)
-//   R.data()->update(1.0, *yn.data(), -1.0, *ynm[0].data(), -dt);
-// }
-// //-------------------------------------------------------
-
-
-// template< ::rompp::ode::ImplicitEnum odeMethod,
-// 	  int numStates,
-// 	  typename state_type,
-// 	  typename scalar_type,
-// 	  core::meta::enable_if_t<
-//   (odeMethod == ::rompp::ode::ImplicitEnum::BDF2)
-//  and ::rompp::core::meta::is_vector_wrapper_tpetra<state_type>::value
-//   > * = nullptr
-// 	 >
-// void time_discrete_residual(const state_type & yn,
-// 			    const std::array<state_type,numStates> & ynm,
-// 			    state_type & R,
-// 			    scalar_type dt){
-//   // // On input: R should contain the application RHS
-//   using namespace ::rompp::ode::coeffs;
-//   // R.data()->update(bdf2<scalar_type>::c2_,
-// 		//    *ynm[1].data(),
-// 		//    -bdf2<scalar_type>::c3_*dt);
-//   const scalar_type oneSc = static_cast<scalar_type>(1);
-//   const scalar_type c2 = bdf2<scalar_type>::c2_;
-//   R.data()->update(c2, *ynm[0].data(), -bdf2<scalar_type>::c3_*dt);
-//   R.data()->update(oneSc, *yn.data(), -bdf2<scalar_type>::c1_, *ynm[1].data(), oneSc);
-// }
-// #endif
