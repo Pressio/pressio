8ba54c28d7e888df3932a41a0282f233f2fc0690
Francesco Rizzi <fnrizzi@sandia.gov>
2019-03-25 16:23:42 +0100

    ode: enable custom residual but standard jacobian policy for implicit steppers

--- packages/ode/src/implicit/steppers/ode_implicit_stepper.hpp
+++ packages/ode/src/implicit/steppers/ode_implicit_stepper.hpp
@@ -7,7 +7,7 @@
 
 namespace rompp{ namespace ode{
 
-// policy is STANDARD
+// both policies are STANDARD
 template<ImplicitEnum whichone,
 	 typename ode_state_type,
 	 typename ode_residual_type,
@@ -111,7 +111,122 @@ private:
 
 
 
-// policy is user defined
+
+// residual is user-defined
+// jacobian is standard
+template<ImplicitEnum whichone,
+	 typename ode_state_type,
+	 typename ode_residual_type,
+	 typename ode_jacobian_type,
+	 typename model_type,
+	 typename aux_stepper_type,
+	 typename residual_policy_type>
+class ImplicitStepper<whichone, ode_state_type, ode_residual_type,
+		      ode_jacobian_type, model_type, aux_stepper_type,
+		      residual_policy_type, void>
+  : public ImplicitStepperBase<
+  ImplicitStepper<whichone, ode_state_type, ode_residual_type,
+		  ode_jacobian_type, model_type,
+		  aux_stepper_type, residual_policy_type, void>
+  >{
+
+  using this_t = ImplicitStepper<whichone, ode_state_type, ode_residual_type,
+				 ode_jacobian_type, model_type,
+				 aux_stepper_type, residual_policy_type, void>;
+  using stepper_base_t = ImplicitStepperBase<this_t>;
+  friend stepper_base_t;
+
+  using mytraits	   = details::traits<this_t>;
+  using scalar_t	   = typename mytraits::scalar_t;
+  using jacobian_pol_t = typename mytraits::jacobian_policy_t;
+
+  // this is the impl class
+  using impl_class_t = typename mytraits::impl_t;
+  impl_class_t myImpl = {};
+
+public:
+  // these need to be here because are detected by solver
+  using state_type = ode_state_type;
+  using residual_type = ode_residual_type;
+  using jacobian_type = ode_jacobian_type;
+
+public:
+  ImplicitStepper() = delete;
+  ~ImplicitStepper() = default;
+
+  // passing: model, initial state, and policies
+  ImplicitStepper(const model_type & model,
+		  const residual_policy_type & resPolicyObj,
+		  const ode_state_type & y0)
+    : myImpl(model, resPolicyObj, jacobian_pol_t(), y0){}
+
+  // passing: model, initial state,
+  // initial residual and policies
+  ImplicitStepper(const model_type & model,
+		  const residual_policy_type & resPolicyObj,
+		  const ode_state_type & y0,
+		  const ode_residual_type & r0)
+    : myImpl(model, resPolicyObj,
+	     jacobian_pol_t(), y0, r0){}
+
+  // passing: model, initial state, aux_stepper
+  template <typename T = aux_stepper_type,
+	    core::meta::enable_if_t<
+	      not std::is_void<T>::value
+	      > * = nullptr>
+  ImplicitStepper(const model_type & model,
+		  const residual_policy_type & resPolicyObj,
+		  const ode_state_type & y0,
+		  T & auxStObj)
+    : myImpl(model, resPolicyObj,
+	     jacobian_pol_t(),
+	     auxStObj, y0){}
+
+public:
+  template<typename step_t, typename solver_type,
+	   typename ... Args>
+  void operator()(ode_state_type & y,
+		  scalar_t t,
+		  scalar_t dt,
+		  step_t step,
+		  solver_type & solver,
+		  Args ... args){
+#ifdef DEBUG_PRINT
+    ::rompp::core::io::print_stdout("\ncurrent ode state is:","\n");
+    ::rompp::core::io::print_core_wrapper(y, std::cout, 'd',-1);
+#endif
+    myImpl.doStep(*this, y, t, dt, step,
+		  solver, std::forward<Args>(args)...);
+  }
+
+private:
+  void residualImpl(const ode_state_type & y,
+		    ode_residual_type & R) const{
+    myImpl.computeResidual(y, R);
+  }
+
+  void jacobianImpl(const ode_state_type & y,
+		    ode_jacobian_type & J) const{
+    myImpl.computeJacobian(y, J);
+  }
+
+  ode_residual_type residualImpl(const ode_state_type & y) const{
+    return 	myImpl.computeResidual(y);
+  }
+
+  ode_jacobian_type jacobianImpl(const ode_state_type & y) const{
+    return myImpl.computeJacobian(y);
+  }
+
+};//end class
+
+
+
+
+
+
+
+// both policies are user-defined
 template<ImplicitEnum whichone,
 	 typename ode_state_type,
 	 typename ode_residual_type,
@@ -150,19 +265,9 @@ public:
   using residual_type = ode_residual_type;
   using jacobian_type = ode_jacobian_type;
 
-#ifdef DEBUG_PRINT
-  int statePrintSize_ = -1;
-#endif
-
 public:
   ImplicitStepper() = delete;
   ~ImplicitStepper() = default;
-
-#ifdef DEBUG_PRINT
-  void setStatePrintSize(int n){
-    statePrintSize_ = n;
-  }
-#endif
 
   // passing: model, initial state, and policies
   ImplicitStepper(const model_type & model,
@@ -206,8 +311,7 @@ public:
 		  Args ... args){
 #ifdef DEBUG_PRINT
     ::rompp::core::io::print_stdout("\ncurrent ode state is:","\n");
-    ::rompp::core::io::print_core_wrapper(y, std::cout, 'd',
-					  statePrintSize_);
+    ::rompp::core::io::print_core_wrapper(y, std::cout, 'd',-1);
 #endif
     myImpl.doStep(*this, y, t, dt, step,
 		  solver, std::forward<Args>(args)...);
--- packages/ode/src/implicit/steppers/ode_implicit_stepper_traits.hpp
+++ packages/ode/src/implicit/steppers/ode_implicit_stepper_traits.hpp
@@ -39,50 +39,117 @@ struct stepsHelper<ImplicitEnum::BDF2>{
 //--------------------------------------------
 
 
-template <typename state_type, typename residual_type,
-	  typename jacobian_type, typename model_type>
+
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model_type
+  >
 struct standardPolHelper{
   using residual_policy_t =
     policy::ImplicitResidualStandardPolicy<state_type, model_type, residual_type>;
   using jacobian_policy_t =
     policy::ImplicitJacobianStandardPolicy<state_type, model_type, jacobian_type>;
 };
+
+template <
+  typename state_type, typename jacobian_type, typename model_type
+  >
+struct standardPolHelper<state_type, void, jacobian_type, model_type>
+{
+  using jacobian_policy_t =
+    policy::ImplicitJacobianStandardPolicy<state_type, model_type, jacobian_type>;
+};
+
+template <
+  typename state_type, typename residual_type, typename model_type
+  >
+struct standardPolHelper<state_type, residual_type, void, model_type>
+{
+  using residual_policy_t =
+    policy::ImplicitResidualStandardPolicy<state_type, model_type, residual_type>;
+};
+
 //--------------------------------------------
 
 
+
+
 // metaf to help define the implementation class type
-template <ImplicitEnum whicone,
-	  typename state_type, typename residual_type,
-	  typename jacobian_type, typename model,
-	  typename aux_stepper_type,  typename res_pol_t,
-	  typename jac_pol_t, typename enable = void>
+template <
+  ImplicitEnum whicone,
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model,
+  typename aux_stepper_type,  typename res_pol_t,
+  typename jac_pol_t, typename enable = void
+  >
 struct implClassHelper;
 
 
-// partial specialize for euler for STANDARD policies
-template <typename state_type, typename residual_type,
-	  typename jacobian_type, typename model>
-struct implClassHelper<ImplicitEnum::Euler, state_type, residual_type,
-		       jacobian_type, model, void, void, void>
-  : standardPolHelper<state_type, residual_type, jacobian_type, model>{
 
+//******************
+//****** EULER *****
+//******************
+
+// EULER with STANDARD policies
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model
+  >
+struct implClassHelper<
+  ImplicitEnum::Euler, state_type, residual_type,
+  jacobian_type, model, void, void, void
+  > : standardPolHelper<state_type, residual_type, jacobian_type, model>
+{
   using base_t = standardPolHelper<state_type, residual_type, jacobian_type, model>;
   using typename base_t::residual_policy_t;
   using typename base_t::jacobian_policy_t;
-  using impl_t = impl::ImplicitEulerStepperImpl<state_type, residual_type, jacobian_type,
-						model, residual_policy_t, jacobian_policy_t>;
+  using impl_t = impl::ImplicitEulerStepperImpl
+    <state_type, residual_type, jacobian_type,
+     model, residual_policy_t, jacobian_policy_t>;
 };
 
 
-// partial specialize for euler for user-defined policies
-template <typename state_type, typename residual_type,
-	  typename jacobian_type, typename model,
-	  typename res_pol_t, typename jac_pol_t>
-struct implClassHelper<ImplicitEnum::Euler, state_type, residual_type,
-		       jacobian_type, model, void, res_pol_t, jac_pol_t,
-		       core::meta::enable_if_t<
-			 !std::is_void<res_pol_t>::value and !std::is_void<jac_pol_t>::value>
-		       >{
+// EULER with:
+// residual = user-defined
+// jacobian = standard
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model,
+  typename res_pol_t
+  >
+struct implClassHelper<
+  ImplicitEnum::Euler, state_type, residual_type,
+  jacobian_type, model, void, res_pol_t, void,
+  core::meta::enable_if_t<
+    !std::is_void<res_pol_t>::value
+    >
+  > : standardPolHelper<state_type, void, jacobian_type, model>
+{
+  using base_t = standardPolHelper<state_type, void, jacobian_type, model>;
+  using typename base_t::jacobian_policy_t;
+
+  using residual_policy_t = res_pol_t;
+
+  using impl_t = impl::ImplicitEulerStepperImpl
+    <state_type, residual_type, jacobian_type,
+     model, residual_policy_t, jacobian_policy_t>;
+};
+
+
+// EULER and user-defined policies
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model,
+  typename res_pol_t, typename jac_pol_t
+  >
+struct implClassHelper<
+  ImplicitEnum::Euler, state_type, residual_type,
+  jacobian_type, model, void, res_pol_t, jac_pol_t,
+  core::meta::enable_if_t<
+    !std::is_void<res_pol_t>::value and !std::is_void<jac_pol_t>::value
+    >
+  >
+{
   using residual_policy_t = res_pol_t;
   using jacobian_policy_t = jac_pol_t;
   using impl_t = impl::ImplicitEulerStepperImpl<state_type, residual_type, jacobian_type,
@@ -90,37 +157,82 @@ struct implClassHelper<ImplicitEnum::Euler, state_type, residual_type,
 };
 
 
-// partial specialize for BDF2 for STANDARD policies
-template <typename state_type, typename residual_type,
-	  typename jacobian_type, typename model, typename aux_step_t>
-struct implClassHelper<ImplicitEnum::BDF2, state_type, residual_type,
-		       jacobian_type, model, aux_step_t, void, void>
-  : standardPolHelper<state_type, residual_type, jacobian_type, model>{
 
+//******************
+//****** BDF2 *****
+//******************
+
+// BDF2 and STANDARD policies
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model,
+  typename aux_step_t
+  >
+struct implClassHelper<
+  ImplicitEnum::BDF2, state_type, residual_type,
+  jacobian_type, model, aux_step_t, void, void
+  > : standardPolHelper<state_type, residual_type, jacobian_type, model>
+{
   using base_t = standardPolHelper<state_type, residual_type, jacobian_type, model>;
   using typename base_t::residual_policy_t;
   using typename base_t::jacobian_policy_t;
-  using impl_t = impl::ImplicitBDF2StepperImpl<state_type, residual_type, jacobian_type, model,
-					       aux_step_t, residual_policy_t, jacobian_policy_t>;
+  using impl_t = impl::ImplicitBDF2StepperImpl
+    <state_type, residual_type, jacobian_type, model,
+     aux_step_t, residual_policy_t, jacobian_policy_t>;
 };
 
 
-// partial specialize for BDF2 for user-defined policies
-template <typename state_type, typename residual_type,
-	  typename jacobian_type, typename model, typename aux_step_t,
-	  typename res_pol_t, typename jac_pol_t>
-struct implClassHelper<ImplicitEnum::BDF2, state_type, residual_type,
-		       jacobian_type, model, aux_step_t, res_pol_t, jac_pol_t,
-		       core::meta::enable_if_t<
-			 !std::is_void<res_pol_t>::value and !std::is_void<jac_pol_t>::value>
-		       >{
+// BDF2 with:
+// residual = user-defined
+// jacobian = standard
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model,
+  typename aux_step_t, typename res_pol_t
+  >
+struct implClassHelper<
+  ImplicitEnum::BDF2, state_type, residual_type,
+  jacobian_type, model, aux_step_t, res_pol_t, void,
+  core::meta::enable_if_t<
+    !std::is_void<res_pol_t>::value
+    >
+  > : standardPolHelper<state_type, void, jacobian_type, model>
+{
+  using base_t = standardPolHelper<state_type, void, jacobian_type, model>;
+  using typename base_t::jacobian_policy_t;
+
+  using residual_policy_t = res_pol_t;
+
+  using impl_t = impl::ImplicitBDF2StepperImpl
+    <state_type, residual_type, jacobian_type,
+     model, aux_step_t, residual_policy_t, jacobian_policy_t>;
+};
+
+
+// BDF2 and user-defined policies
+template <
+  typename state_type, typename residual_type,
+  typename jacobian_type, typename model, typename aux_step_t,
+  typename res_pol_t, typename jac_pol_t
+  >
+struct implClassHelper<
+  ImplicitEnum::BDF2, state_type, residual_type,
+  jacobian_type, model, aux_step_t, res_pol_t, jac_pol_t,
+  core::meta::enable_if_t<
+    !std::is_void<res_pol_t>::value and
+    !std::is_void<jac_pol_t>::value
+    >
+  >
+{
   using residual_policy_t = res_pol_t;
   using jacobian_policy_t = jac_pol_t;
-  using impl_t = impl::ImplicitBDF2StepperImpl<state_type, residual_type, jacobian_type,
-					       model, aux_step_t, res_pol_t, jac_pol_t>;
+  using impl_t = impl::ImplicitBDF2StepperImpl
+    <state_type, residual_type, jacobian_type,
+     model, aux_step_t, res_pol_t, jac_pol_t>;
 };
 
 }//end namespace impl
+
 
 
 namespace details{
@@ -137,16 +249,18 @@ template<
   typename jacobian_policy_type
   >
 struct traits<
-  ImplicitStepper<whichone,
-		  state_type, residual_type, jacobian_type,
-		  model_type, aux_stepper_type,
-		  residual_policy_type, jacobian_policy_type
-		  >
+  ImplicitStepper<
+    whichone,
+    state_type, residual_type, jacobian_type,
+    model_type, aux_stepper_type,
+    residual_policy_type, jacobian_policy_type
+    >
   > : impl::orderHelper<whichone>,
       impl::stepsHelper<whichone>,
       impl::implClassHelper<whichone, state_type, residual_type, jacobian_type,
 			    model_type, aux_stepper_type,
-			    residual_policy_type, jacobian_policy_type>{
+			    residual_policy_type, jacobian_policy_type>
+{
 
   static constexpr ode::ImplicitEnum enum_id = whichone;
   static constexpr bool is_implicit = true;
