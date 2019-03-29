a9f9f52198a5c2d5a458039a507cb2a5b3dc64db
Francesco Rizzi <fnrizzi@sandia.gov>
2019-03-29 20:55:34 +0100

    apps: adding steady 2D adv-diff and fom test

---
+++ packages/apps/src/steady_adv_diff2d/apps_steady_adv_diff_2d_epetra.hpp
@@ -0,0 +1,214 @@
+
+#ifndef ROMPP_APPS_STEADY_ADV_DIFF_2D_EPETRA_HPP_
+#define ROMPP_APPS_STEADY_ADV_DIFF_2D_EPETRA_HPP_
+
+#include "../../../CORE_ALL"
+#include "Epetra_MpiComm.h"
+#include <Epetra_config.h>
+#include "Epetra_Map.h"
+#include "Epetra_IntVector.h"
+#include "Epetra_SerialDenseVector.h"
+#include "Epetra_Vector.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_Time.h"
+#include "AztecOO_config.h"
+#include "AztecOO.h"
+#include <cmath>
+
+namespace rompp{ namespace apps{
+/*
+  See this:
+http://demonstrations.wolfram.com/SteadyStateTwoDimensionalConvectionDiffusionEquation/
+
+  Solve this:
+  (Pr*Re)^-1 * (d2T/dx2 + d2T/dy2) - u dT/dx -v dT/dy = 0
+
+  Domain (0,1) times (0,2)
+  Dirichlet BC on left wall and right wall
+  Tleft = 0 , Tright = 1
+  homogeneous Neumann on top and bottom
+
+  Use 2nd order FD, with following grid:
+
+  o x x x x o
+  o x x x x o
+  o x x x x o
+  o x x x x o
+
+  The x denotes an unknown. o : known values
+  Enumeration of unknowns is done left-right from bottom to top
+  as follows:
+
+    ...
+  o x x x x o
+
+    4 5 6 7
+  o x x x x o
+
+    0 1 2 3
+  o x x x x o
+
+  MPI decomp is done (simply) by splitting blocks of the domains
+  vertically, and ranks go from 0 to n_ranks-1 starting from bottom.
+ */
+
+class SteadyAdvDiff2dEpetra{
+protected:
+  using nativeVec = Epetra_Vector;
+  template<typename T> using rcp = std::shared_ptr<T>;
+  using nativeMatrix	= Epetra_CrsMatrix;
+
+public:
+  /* these types exposed because need to be detected */
+  using scalar_type	= double;
+  using state_type	= Epetra_Vector;
+  using residual_type	= state_type;
+
+public:
+  SteadyAdvDiff2dEpetra(Epetra_MpiComm & comm,
+			int Nx, int Ny,
+			scalar_type Pr = 2.,
+			scalar_type Re = 30.,
+			scalar_type Tleft = 0.0,
+			scalar_type Tright = 1.0)
+    : comm_(comm), Nx_{Nx}, Ny_{Ny},
+      NxDof_{Nx-2}, NyDof_{Ny}, // because Dirichlet in x, Neumann in y
+      Pr_{Pr}, Re_{Re}, invPrRe_{1./(Pr_*Re_)},
+      Tleft_{Tleft}, Tright_{Tright},
+      dx_{Lx_/(Nx-1)},
+      dy_{Ly_/(Ny-1)},
+      dxSqInv_{1.0/(dx_*dx_)},
+      dySqInv_{1.0/(dy_*dy_)},
+      dx2Inv_{1./dx_},
+      dy2Inv_{1./dy_}
+  {
+    std::cout << dx_ << " " << dy_ << std::endl;
+  }
+
+private:
+  void localIDToLiLj(int ID, int & li, int & lj) const{
+    lj = ID/NxDof_;
+    li = ID % NxDof_;
+  }
+  void globalIDToGiGj(int ID, int & gi, int & gj) const{
+    gj = ID/NxDof_;
+    gi = ID % NxDof_;
+  }
+
+
+public:
+  void createMap();
+  Epetra_Map const & getDataMap(){ return *myMap_; };
+  void setup();
+  void assembleMatrix() const;
+  void fillRhs() const;
+  void solve();
+
+  void printStateToFile(std::string fileName){
+    std::ofstream file;
+    file.open( fileName );
+    for(auto i=0; i < dofPerProc_; i++){
+      file << std::fixed << std::setprecision(14) <<
+	(*x_)[i] << " " << (*y_)[i] << " " << (*T_)[i];
+      file << std::endl;
+    }
+    file.close();
+  }
+
+  int getNumGlobalDofs() const{
+    return numGlobalDof_;
+  }
+
+  std::shared_ptr<Epetra_Vector> getState() const {
+    return T_;
+  }
+
+protected:
+  //  constexpr scalar_type PI = 3.14159265358979323846;
+  const int maxNonZeroPerRow_ = 5;
+  const scalar_type solveTolerance_ = 1e-12;
+  scalar_type Lx_ = 1.0;
+  scalar_type Ly_ = 2.0;
+  std::array<scalar_type,2> xAxis_{0., 1.};
+  std::array<scalar_type,2> yAxis_{0., 2.};
+
+  Epetra_MpiComm & comm_;
+  // physical grid points
+  int Nx_{};
+  int Ny_{};
+  // dof's which are not same as physical
+  // we consider inner grid along x, but
+  // the full grid along y because of neumann BC
+  int NxDof_{};
+  int NyDof_{};
+
+  scalar_type Pr_{2.};
+  scalar_type Re_{30.};
+  scalar_type invPrRe_{};
+  scalar_type Tleft_{0.0};
+  scalar_type Tright_{1.0};
+
+  scalar_type dx_{};
+  scalar_type dy_{};
+  scalar_type dxSqInv_{};
+  scalar_type dySqInv_{};
+  scalar_type dx2Inv_{};
+  scalar_type dy2Inv_{};
+
+  rcp<Epetra_Map> myMap_;
+  int numGlobalDof_;
+  int *MyGlobalDof_;
+  int dofPerProc_;
+
+  rcp<nativeVec> x_;
+  rcp<nativeVec> y_;
+  rcp<nativeVec> u_;
+  rcp<nativeVec> v_;
+  mutable rcp<nativeMatrix> A_;
+  mutable rcp<nativeVec> T_;
+  mutable rcp<nativeVec> f_;
+
+};
+
+}} //namespace rompp::apps
+#endif
+
+
+
+
+
+// public:
+//   void residual(const state_type & u,
+// 		residual_type & rhs) const{
+//     calculateLinearSystem();
+//     calculateForcingTerm();
+//     A_->Multiply(false, u, rhs);
+//     // now, rhs = A*u so we just subtract f to obtain residual
+//     rhs.Update(-1., (*f_), 1.0);
+//   }
+
+//   residual_type residual(const state_type & u) const{
+//     Epetra_Vector R(*contigMap_);
+//     residual(u,R);
+//     return R;
+//   }
+
+//   // computes: C = Jac B where B is a multivector
+//   void applyJacobian(const state_type & y,
+// 		     const Epetra_MultiVector & B,
+// 		     Epetra_MultiVector & C) const
+//   {
+//     assert( A_->NumGlobalCols() == B.GlobalLength() );
+//     assert( C.GlobalLength() == A_->NumGlobalRows() );
+//     assert( C.NumVectors() == B.NumVectors() );
+//     calculateLinearSystem();
+//     A_->Multiply(false, B, C);
+//   }
+
+//   // computes: A = Jac B where B is a multivector
+//   Epetra_MultiVector applyJacobian(const state_type & y,
+//   				   const Epetra_MultiVector & B) const{
+//     Epetra_MultiVector C( *contigMap_, B.NumVectors() );
+//     applyJacobian(y, B, C);
+//     return C;
+//   }
---
+++ packages/apps/test/steady_adv_diff_2d/rom/epetra/lspg_normal_eq/main.cc
@@ -0,0 +1,93 @@
+
+#include "CORE_ALL"
+#include "SOLVERS_NONLINEAR"
+#include "ROM_LSPG_STEADY"
+#include "APPS_STEADYADVDIFF1D"
+#include "utils_epetra.hpp"
+#include "../../../fom/fom_gold_states.hpp"
+
+int main(int argc, char *argv[]){
+  using fom_t		= rompp::apps::SteadyAdvDiff1dEpetra;
+  using scalar_t	= typename fom_t::scalar_type;
+  using eig_dyn_vec	= Eigen::Matrix<scalar_t, -1, 1>;
+  using lspg_state_t	= rompp::core::Vector<eig_dyn_vec>;
+
+  using decoder_jac_t	= rompp::core::MultiVector<Epetra_MultiVector>;
+  using decoder_t	= rompp::rom::LinearDecoder<decoder_jac_t>;
+
+  //-------------------------------
+  // MPI init
+  MPI_Init(&argc,&argv);
+  int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  Epetra_MpiComm Comm(MPI_COMM_WORLD);
+  assert(Comm.NumProc() == 2);
+
+  //-------------------------------
+  //Parameters: diffusion, advection, expf
+  std::vector<scalar_t> mu{-7, 1, 1};
+  //1D spatial domain, xL, xR
+  std::vector<scalar_t> domain{0, 2.0, 0.01};
+  //Left and right boundary conditions
+  std::vector<scalar_t> bc1D{0,0.25};
+  //Create object
+  fom_t  appObj(Comm, mu, domain, bc1D);
+  appObj.setup();
+
+  // number of degrees of freedom
+  const int numDof = appObj.getNumGlobalNodes();
+
+  // read the jacobian of the decoder
+  constexpr int romSize = 5;
+  // store modes computed before from file
+  decoder_jac_t phi =
+    rompp::apps::test::epetra::readBasis("basis.txt", romSize, numDof,
+  					 Comm, appObj.getDataMap());
+  //print to terminal the basis
+  //phi.data()->Print(std::cout);
+  // decoder object
+  decoder_t decoderObj(phi);
+
+  // my reference state
+  auto yRef = appObj.getState();
+  yRef->PutScalar( static_cast<scalar_t>(0) );
+
+  // define ROM state
+  lspg_state_t yROM(romSize);
+  // initialize to zero (for now)
+  yROM.putScalar(0.0);
+
+  // define LSPG type
+  using lspg_problem_type = rompp::rom::DefaultLSPGSteadyTypeGenerator<
+    fom_t, decoder_t, lspg_state_t>;
+  rompp::rom::LSPGSteadyProblemGenerator<lspg_problem_type> lspgProblem(
+      appObj, *yRef, decoderObj, yROM);
+
+  // GaussNewton solver
+  // hessian comes up in GN solver, it is (J phi)^T (J phi)
+  // rom is solved using eigen, hessian is wrapper of eigen matrix
+  using eig_dyn_mat	 = Eigen::Matrix<scalar_t, -1, -1>;
+  using hessian_t	 = rompp::core::Matrix<eig_dyn_mat>;
+  using solver_tag	 = rompp::solvers::linear::LSCG;
+  using converged_when_t = rompp::solvers::iterative::default_convergence;
+  using rom_system_t = typename lspg_problem_type::lspg_system_t;
+  using gnsolver_t	 = rompp::solvers::iterative::GaussNewton<
+    scalar_t, solver_tag, rompp::solvers::EigenIterative,
+    converged_when_t, rom_system_t, hessian_t>;
+  gnsolver_t solver(lspgProblem.systemObj_, yROM);
+  solver.setTolerance(1e-6);
+  solver.setMaxIterations(200);
+  solver.solve(lspgProblem.systemObj_, yROM);
+
+  // compute the fom corresponding to our rom final state
+  auto yFomFinal = lspgProblem.yFomReconstructor_(yROM);
+  yFomFinal.data()->Print(std::cout << std::setprecision(14));
+
+  // /* if there is a reproducing test we can do, let's do it and we can
+  //  * compare with the FOM solution.
+  //  * Otherwise, we can compare ROM solution in some other way (?)
+  //  * I leave the final check empty for now.
+  //  */
+
+  MPI_Finalize();
+  return 0;
+}
