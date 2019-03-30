89756d40b72c60ffe7fa3e5a044e02c46154968f
Francesco Rizzi <fnrizzi@sandia.gov>
2019-03-30 21:29:09 +0100

    apps: add reproducing test for steady lspg for adv-diff 2d

--- packages/apps/test/steady_adv_diff_2d/rom/epetra/lspg_normal_eq/main.cc
+++ packages/apps/test/steady_adv_diff_2d/rom/epetra/lspg_normal_eq/main.cc
@@ -2,65 +2,74 @@
 #include "CORE_ALL"
 #include "SOLVERS_NONLINEAR"
 #include "ROM_LSPG_STEADY"
-#include "APPS_STEADYADVDIFF1D"
+#include "APPS_STEADYADVDIFF2D"
 #include "utils_epetra.hpp"
-#include "../../../fom/fom_gold_states.hpp"
 
 int main(int argc, char *argv[]){
-  using fom_t		= rompp::apps::SteadyAdvDiff1dEpetra;
-  using scalar_t	= typename fom_t::scalar_type;
-  using eig_dyn_vec	= Eigen::Matrix<scalar_t, -1, 1>;
-  using lspg_state_t	= rompp::core::Vector<eig_dyn_vec>;
+  using true_fom_t	= rompp::apps::SteadyAdvDiff2dEpetra;
+  using fom_adapter_t	= rompp::apps::SteadyAdvDiff2dEpetraRomAdapter;
+  using scalar_t	= typename fom_adapter_t::scalar_type;
+  using native_state	= typename fom_adapter_t::state_type;
 
-  using decoder_jac_t	= rompp::core::MultiVector<Epetra_MultiVector>;
-  using decoder_t	= rompp::rom::LinearDecoder<decoder_jac_t>;
-
-  //-------------------------------
-  // MPI init
+  // MPI
   MPI_Init(&argc,&argv);
   int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   Epetra_MpiComm Comm(MPI_COMM_WORLD);
-  assert(Comm.NumProc() == 2);
+  //assert(Comm.NumProc() == 2);
 
-  //-------------------------------
-  //Parameters: diffusion, advection, expf
-  std::vector<scalar_t> mu{-7, 1, 1};
-  //1D spatial domain, xL, xR
-  std::vector<scalar_t> domain{0, 2.0, 0.01};
-  //Left and right boundary conditions
-  std::vector<scalar_t> bc1D{0,0.25};
-  //Create object
-  fom_t  appObj(Comm, mu, domain, bc1D);
+  // we run the FOM and LSPG for same values of parameters
+  constexpr scalar_t Pr = 2.48875378592597;
+  constexpr scalar_t Re = 41.7029840887032;
+
+  // the discretization to use for solver
+  const int Nx = 11, Ny = Nx*2-1;
+  // we solve for inner grid along x
+  const int numDof = (Nx-2)*Ny;
+
+  // -------------------------
+  // run FOM model
+
+  true_fom_t  appObj(Comm, Nx, Ny, Pr, Re);
   appObj.setup();
+  appObj.assembleMatrix();
+  appObj.fillRhs();
+  appObj.solve();
 
-  // number of degrees of freedom
-  const int numDof = appObj.getNumGlobalNodes();
+  rompp::core::Vector<native_state> yFom(*appObj.getState());
+
+  // -------------------------
+  // LSPG ROM
+  using native_state	= typename fom_adapter_t::state_type;
+  using eig_dyn_vec	= Eigen::Matrix<scalar_t, -1, 1>;
+  using lspg_state_t	= rompp::core::Vector<eig_dyn_vec>;
+  using decoder_jac_t	= rompp::core::MultiVector<Epetra_MultiVector>;
+  using decoder_t	= rompp::rom::LinearDecoder<decoder_jac_t>;
+
+  // app object for running rom
+  fom_adapter_t  appObjROM(Comm, Nx, Ny, Pr, Re);
 
   // read the jacobian of the decoder
   constexpr int romSize = 5;
   // store modes computed before from file
-  decoder_jac_t phi =
+  const decoder_jac_t phi =
     rompp::apps::test::epetra::readBasis("basis.txt", romSize, numDof,
-  					 Comm, appObj.getDataMap());
-  //print to terminal the basis
-  //phi.data()->Print(std::cout);
+  					 Comm, appObjROM.getDataMap());
   // decoder object
   decoder_t decoderObj(phi);
 
   // my reference state
-  auto yRef = appObj.getState();
+  auto yRef = appObjROM.getState();
   yRef->PutScalar( static_cast<scalar_t>(0) );
 
-  // define ROM state
+  // define ROM state and set to zero
   lspg_state_t yROM(romSize);
-  // initialize to zero (for now)
   yROM.putScalar(0.0);
 
   // define LSPG type
   using lspg_problem_type = rompp::rom::DefaultLSPGSteadyTypeGenerator<
-    fom_t, decoder_t, lspg_state_t>;
+    fom_adapter_t, decoder_t, lspg_state_t>;
   rompp::rom::LSPGSteadyProblemGenerator<lspg_problem_type> lspgProblem(
-      appObj, *yRef, decoderObj, yROM);
+      appObjROM, *yRef, decoderObj, yROM);
 
   // GaussNewton solver
   // hessian comes up in GN solver, it is (J phi)^T (J phi)
@@ -69,24 +78,23 @@ int main(int argc, char *argv[]){
   using hessian_t	 = rompp::core::Matrix<eig_dyn_mat>;
   using solver_tag	 = rompp::solvers::linear::LSCG;
   using converged_when_t = rompp::solvers::iterative::default_convergence;
-  using rom_system_t = typename lspg_problem_type::lspg_system_t;
+  using rom_system_t	 = typename lspg_problem_type::lspg_system_t;
   using gnsolver_t	 = rompp::solvers::iterative::GaussNewton<
     scalar_t, solver_tag, rompp::solvers::EigenIterative,
     converged_when_t, rom_system_t, hessian_t>;
   gnsolver_t solver(lspgProblem.systemObj_, yROM);
-  solver.setTolerance(1e-6);
+  solver.setTolerance(1e-13);
   solver.setMaxIterations(200);
   solver.solve(lspgProblem.systemObj_, yROM);
 
-  // compute the fom corresponding to our rom final state
-  auto yFomFinal = lspgProblem.yFomReconstructor_(yROM);
-  yFomFinal.data()->Print(std::cout << std::setprecision(14));
-
-  // /* if there is a reproducing test we can do, let's do it and we can
-  //  * compare with the FOM solution.
-  //  * Otherwise, we can compare ROM solution in some other way (?)
-  //  * I leave the final check empty for now.
-  //  */
+  // reconstruct the fom corresponding to our rom final state
+  auto yFomApprox = lspgProblem.yFomReconstructor_(yROM);
+  /* the ROM is run for a parameter point that was used to generate
+   * the basis, so we should recover the FOM solution exactly */
+  auto errorVec(yFom); errorVec = yFom - yFomApprox;
+  const auto norm2err = rompp::core::ops::norm2(errorVec);
+  assert( norm2err < 1e-12 );
+  std::cout << std::setprecision(15) << norm2err << std::endl;
 
   MPI_Finalize();
   return 0;
