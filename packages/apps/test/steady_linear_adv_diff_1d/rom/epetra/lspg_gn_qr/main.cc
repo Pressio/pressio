d668a8464597c060e491d8cfa3c819df4a24d73a
Francesco Rizzi <fnrizzi@sandia.gov>
2019-04-03 13:19:39 +0200

    apps: adding QR case for steady lspg

--- packages/apps/test/steady_linear_adv_diff_1d/rom/epetra/lspg_gn_qr/main.cc
+++ packages/apps/test/steady_linear_adv_diff_1d/rom/epetra/lspg_gn_qr/main.cc
@@ -70,20 +70,18 @@ int main(int argc, char *argv[]){
   rompp::rom::LSPGSteadyProblemGenerator<lspg_problem_type> lspgProblem(
       appObj, *yRef, decoderObj, yROM);
 
+  using rom_jac_t     = typename lspg_problem_type::lspg_matrix_t;
+
   // GaussNewton solver
-  // hessian comes up in GN solver, it is (J phi)^T (J phi)
-  // rom is solved using eigen, hessian is wrapper of eigen matrix
-  using eig_dyn_mat	 = Eigen::Matrix<scalar_t, -1, -1>;
-  using hessian_t	 = rompp::core::Matrix<eig_dyn_mat>;
-  using solver_tag	 = rompp::solvers::linear::direct::ColPivHouseholderQR;
+  using rom_jac_t     = typename lspg_problem_type::lspg_matrix_t;
+  using qr_algo = rompp::qr::TSQR;
+  using qr_type = rompp::qr::QRSolver<rom_jac_t, qr_algo>;
   using converged_when_t = rompp::solvers::iterative::default_convergence;
-  using rom_system_t = typename lspg_problem_type::lspg_system_t;
-  using gnsolver_t	 = rompp::solvers::iterative::GaussNewton<
-    scalar_t, solver_tag, rompp::solvers::EigenDirect,
-    converged_when_t, rom_system_t, hessian_t>;
-  gnsolver_t solver(lspgProblem.systemObj_, yROM);
+  using gnsolver_t  = rompp::solvers::iterative::GaussNewtonQR<
+         scalar_t, qr_type, converged_when_t>;
+  gnsolver_t solver;
   solver.setTolerance(1e-11);
-  solver.setMaxIterations(30);
+  solver.setMaxIterations(2);
   solver.solve(lspgProblem.systemObj_, yROM);
 
   // compute the fom corresponding to our rom final state
