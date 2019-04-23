c1c9b0877404a083ced19aeb8028f3f9a7a51728
Francesco Rizzi <fnrizzi@sandia.gov>
2019-04-23 16:15:19 +0200

    adding another constant in core

--- packages/core/src/core_static_constants.hpp
+++ packages/core/src/core_static_constants.hpp
@@ -13,13 +13,18 @@ struct constants{
   static constexpr int dynamic = -1;
 
   template <typename scalar_t = double>
+  static constexpr scalar_t zero(){
+    return static_cast<scalar_t>(0);
+  }
+
+  template <typename scalar_t = double>
   static constexpr scalar_t one(){
     return static_cast<scalar_t>(1);
   }
 
   template <typename scalar_t = double>
-  static constexpr scalar_t zero(){
-    return static_cast<scalar_t>(0);
+  static constexpr scalar_t two(){
+    return static_cast<scalar_t>(2);
   }
 
 };
