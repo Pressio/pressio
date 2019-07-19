d2100b5afb7e37f2b5cc5ca4d759e324699c0699
Francesco Rizzi <fnrizzi@sandia.gov>
2019-07-19 15:38:55 +0200

    ode/meta: cleanup metaf

--- packages/ode/src/meta/ode_model_has_all_needed_velocity_methods.hpp
+++ packages/ode/src/meta/ode_model_has_all_needed_velocity_methods.hpp
@@ -18,9 +18,10 @@ struct has_velocity_method_callable_with_two_args<
     !std::is_void<
       decltype(
 	       std::declval<T>().velocity(
-            std::declval<a_t const&>(),
-					  std::declval<b_t &>())
-	   )
+					  std::declval<a_t const&>(),
+					  std::declval<b_t>()
+					  )
+	       )
       >::value
     >
   > : std::true_type{};
@@ -40,7 +41,7 @@ struct has_velocity_method_callable_with_three_args<
 	       std::declval<T>().velocity(
 					  std::declval<a_t const&>(),
 					  std::declval<b_t &>(),
-					  std::declval<c_t &>()
+					  std::declval<c_t>()
 					  )
 	   )
       >::value
@@ -50,43 +51,43 @@ struct has_velocity_method_callable_with_three_args<
 //---------------------------------------------------------------
 
 
-template<typename model_type,
-	 typename state_type,
-	 typename velocity_type,
-	 typename scalar_type,
-	 typename enable = void>
+template<
+  typename model_type,
+  typename state_type,
+  typename velocity_type,
+  typename scalar_type,
+  typename enable = void
+  >
 struct model_has_needed_velocity_methods : std::false_type{};
 
-template<typename model_type,
-	 typename state_type,
-	 typename velocity_type,
-	 typename scalar_type>
+template<
+  typename model_type,
+  typename state_type,
+  typename velocity_type,
+  typename scalar_type
+  >
 struct model_has_needed_velocity_methods<
   model_type, state_type, velocity_type, scalar_type,
-  typename std::enable_if<
-   // has method with 2 arguments,
+  mpl::enable_if_t<
+    // has method with 2 arguments,
     has_velocity_method_callable_with_two_args<
-     model_type, state_type, scalar_type
-     >::value and
-    // containers::meta::is_detected<
-    // has_velocity_method_callable_with_two_args,
-    //  model_type, state_type, scalar_type
-    //  >::value and
-   // has velocity method with 3 arguments
-    /*containers::meta::is_detected<*/
-     has_velocity_method_callable_with_three_args<
-     model_type, state_type, velocity_type, scalar_type
-     >::value and
-   // method with 2 arguments returns a velocity_type
-   std::is_same<
+      model_type, state_type, scalar_type
+      >::value and
+    // has velocity method with 3 arguments
+    has_velocity_method_callable_with_three_args<
+      model_type, state_type, velocity_type, scalar_type
+      >::value and
+    // method with 2 arguments returns a velocity_type
+    mpl::is_same<
       velocity_type,
       decltype(
 	       std::declval<model_type>().velocity
-	       (std::declval<state_type const&>(),
-		std::declval<scalar_type &>())
-	   )
+	       (
+		std::declval<state_type const&>(),
+		std::declval<scalar_type>())
+	       )
       >::value
-  >::type
+    >
   > : std::true_type{};
 
 
