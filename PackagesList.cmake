c23974f1ca613a270e5666f92fd8cbab0723bc81
Francesco Rizzi <fnrizzi@sandia.gov>
2019-09-13 10:38:30 +0200

    apps license done

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -1,12 +1,12 @@
 
 TRIBITS_REPOSITORY_DEFINE_PACKAGES(
-  # TRIBITS wants the order in which you list packages here matching dependencies. 
+  # TRIBITS wants the order in which you list packages here matching dependencies.
   # Dependencies should be read from top to bottom.
   # Packages listed lower should depend on those lister higher.
   mpl		packages/mpl PT
   utils		packages/utils PT
   containers	packages/containers PT
-  apps    packages/apps PT
+  apps		packages/apps PT
   qr		packages/qr PT
   svd		packages/svd PT
   optimization	packages/optimization PT
