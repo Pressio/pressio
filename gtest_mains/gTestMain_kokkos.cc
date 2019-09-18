db93282e0c4521a93424746b4a863130a3e8e001
Francesco Rizzi <fnrizzi@sandia.gov>
2019-09-18 18:36:01 +0200

    fix gtest mains to avoid segfault for custom gtest environment

--- gtest_mains/gTestMain_kokkos.cc
+++ gtest_mains/gTestMain_kokkos.cc
@@ -4,23 +4,23 @@
 #include <Kokkos_Core.hpp>
 #include <memory>
 
-struct KokkosEnv : public ::testing::Environment{
-  int argc_;
-  char ** argv_;
+// struct KokkosEnv : public ::testing::Environment{
+//   int argc_;
+//   char ** argv_;
 
-  KokkosEnv(int argc, char **argv)
-    : argc_(argc), argv_(argv){}
+//   KokkosEnv(int argc, char **argv)
+//     : argc_(argc), argv_(argv){}
 
-  virtual void SetUp() {
-    Kokkos::initialize (argc_, argv_);
-  }
+//   virtual void SetUp() {
+//     Kokkos::initialize (argc_, argv_);
+//   }
 
-  virtual void TearDown() {
-    Kokkos::finalize();
-  }
+//   virtual void TearDown() {
+//     Kokkos::finalize();
+//   }
 
-  virtual ~KokkosEnv() {}
-};
+//   virtual ~KokkosEnv() {}
+// };
 
 
 int main(int argc, char *argv[])
@@ -29,9 +29,11 @@ int main(int argc, char *argv[])
   //  ::testing::InitGoogleMock(&argc,argv);
   int err = 0;
   {
-    std::unique_ptr<KokkosEnv> envPtr(new KokkosEnv(argc, argv));
-    ::testing::AddGlobalTestEnvironment(envPtr.get());
+    // std::unique_ptr<KokkosEnv> envPtr(new KokkosEnv(argc, argv));
+    // ::testing::AddGlobalTestEnvironment(envPtr.get());
+    Kokkos::initialize (argc, argv);
     err = RUN_ALL_TESTS();
+    Kokkos::finalize();
   }
   return err;
 }
--- gtest_mains/gTestMain_mpi.cc
+++ gtest_mains/gTestMain_mpi.cc
@@ -4,39 +4,47 @@
 #include <mpi.h>
 #include <memory>
 
-struct MPIEnv : public ::testing::Environment{
+// struct MPIEnv : public ::testing::Environment{
 
-  int rank_;
-  int argc_;
-  char ** argv_;
+//   int rank_;
+//   int argc_;
+//   char ** argv_;
 
-  MPIEnv(int argc, char **argv)
-    : argc_(argc), argv_(argv){}
+//   MPIEnv(int argc, char **argv)
+//     : argc_(argc), argv_(argv){}
 
-  virtual void SetUp() {
-    int mpiError = MPI_Init(&argc_, &argv_);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank_);
-    ASSERT_FALSE(mpiError);
-  }
+//   virtual void SetUp() {
+//     int mpiError = MPI_Init(&argc_, &argv_);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank_);
+//     ASSERT_FALSE(mpiError);
+//   }
 
-  virtual void TearDown() {
-    int mpiError = MPI_Finalize();
-    ASSERT_FALSE(mpiError);
-  }
+//   virtual void TearDown() {
+//     int mpiError = MPI_Finalize();
+//     ASSERT_FALSE(mpiError);
+//   }
 
-  virtual ~MPIEnv() {}
-};
+//   virtual ~MPIEnv() {}
+// };
 
 
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc,argv);
-  //::testing::InitGoogleMock(&argc,argv);
   int err = 0;
   {
-    std::unique_ptr<MPIEnv> envPtr(new MPIEnv(argc, argv));
-    ::testing::AddGlobalTestEnvironment(envPtr.get());
+    // std::unique_ptr<MPIEnv> envPtr(new MPIEnv(argc, argv));
+    // ::testing::AddGlobalTestEnvironment(envPtr.get());
+
+    int rank = {};
+    int mpiError = MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    //ASSERT_FALSE(mpiError);
+
     err = RUN_ALL_TESTS();
+
+    mpiError = MPI_Finalize();
+    //ASSERT_FALSE(mpiError);
   }
   return err;
 }
