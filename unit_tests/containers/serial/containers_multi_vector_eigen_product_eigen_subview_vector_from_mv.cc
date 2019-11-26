d2e3631e8cfd686177ad34baead1121fa89e9385
Francesco Rizzi <fnrizzi@sandia.gov>
2019-11-26 13:34:57 +0100

    comment unit test wrongly added

--- unit_tests/containers/serial/containers_multi_vector_eigen_product_eigen_subview_vector_from_mv.cc
+++ unit_tests/containers/serial/containers_multi_vector_eigen_product_eigen_subview_vector_from_mv.cc
@@ -59,38 +59,38 @@ TEST(containers_multi_vector_serial_eigen_dynamic_class,
   }
 }
 
-TEST(containers_multi_vector_serial_eigen_dynamic_class,
-     productWithTargetColVectorEigenMVector){
+// TEST(containers_multi_vector_serial_eigen_dynamic_class,
+//      productWithTargetColVectorEigenMVector){
 
-  //construct multivector
-  myMV_t A(6,3);
-  A(0,0) = 1.; A(0,1) = 2.; A(0,2) = 3.;
-  A(1,0) = 3.; A(1,1) = 2.; A(1,2) = 1.;
-  A(2,0) = 0.; A(2,1) = 0.; A(2,2) = 1.;
-  A(3,0) = 0.; A(3,1) = 1.; A(3,2) = 0.;
-  A(4,0) = 1.; A(4,1) = 0.; A(4,2) = 0.;
-  A(5,0) = 0.; A(5,1) = 1.; A(5,2) = 1.;
+//   //construct multivector
+//   myMV_t A(6,3);
+//   A(0,0) = 1.; A(0,1) = 2.; A(0,2) = 3.;
+//   A(1,0) = 3.; A(1,1) = 2.; A(1,2) = 1.;
+//   A(2,0) = 0.; A(2,1) = 0.; A(2,2) = 1.;
+//   A(3,0) = 0.; A(3,1) = 1.; A(3,2) = 0.;
+//   A(4,0) = 1.; A(4,1) = 0.; A(4,2) = 0.;
+//   A(5,0) = 0.; A(5,1) = 1.; A(5,2) = 1.;
 
-  // eigen mvB
-  pressio::containers::MultiVector<Eigen::MatrixXd> B(3,2);
-  B(0,0) = 1.; B(0,1) = 2.;
-  B(1,0) = 1.; B(1,1) = 2.;
-  B(2,0) = 1.; B(2,1) = 2.;
+//   // eigen mvB
+//   pressio::containers::MultiVector<Eigen::MatrixXd> B(3,2);
+//   B(0,0) = 1.; B(0,1) = 2.;
+//   B(1,0) = 1.; B(1,1) = 2.;
+//   B(2,0) = 1.; B(2,1) = 2.;
 
-  {
-    auto c1 = pressio::containers::ops::product(A, B, 0);
-    ASSERT_EQ( c1.size(), 6 );
-    EXPECT_DOUBLE_EQ( c1(0), 6.);
-    EXPECT_DOUBLE_EQ( c1(1), 6.);
-    EXPECT_DOUBLE_EQ( c1(2), 1.);
-    EXPECT_DOUBLE_EQ( c1(3), 1.);
-    EXPECT_DOUBLE_EQ( c1(4), 1.);
-    EXPECT_DOUBLE_EQ( c1(5), 2.);
+//   {
+//     auto c1 = pressio::containers::ops::product(A, B, 0);
+//     ASSERT_EQ( c1.size(), 6 );
+//     EXPECT_DOUBLE_EQ( c1(0), 6.);
+//     EXPECT_DOUBLE_EQ( c1(1), 6.);
+//     EXPECT_DOUBLE_EQ( c1(2), 1.);
+//     EXPECT_DOUBLE_EQ( c1(3), 1.);
+//     EXPECT_DOUBLE_EQ( c1(4), 1.);
+//     EXPECT_DOUBLE_EQ( c1(5), 2.);
 
-    pressio::containers::Vector<Eigen::VectorXd> c(6);
-    pressio::containers::ops::product(A, B, c, 0);
-    for (auto i=0; i<c1.size(); i++)
-      EXPECT_DOUBLE_EQ( c(i), c1(i));
-  }
+//     pressio::containers::Vector<Eigen::VectorXd> c(6);
+//     pressio::containers::ops::product(A, B, c, 0);
+//     for (auto i=0; i<c1.size(); i++)
+//       EXPECT_DOUBLE_EQ( c(i), c1(i));
+//   }
 
-}
+// }
