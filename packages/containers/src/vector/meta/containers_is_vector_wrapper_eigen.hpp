9171843bf80cd3025faced684bea0c59eda6ee40
Francesco Rizzi <fnrizzi@sandia.gov>
2019-12-16 23:42:39 +0100

    adding metaf to check for dynamic vector wrapper eigen

--- packages/containers/src/vector/meta/containers_is_vector_wrapper_eigen.hpp
+++ packages/containers/src/vector/meta/containers_is_vector_wrapper_eigen.hpp
@@ -71,5 +71,22 @@ struct is_vector_wrapper_eigen<
        >
   > : std::true_type{};
 
+
+
+template <typename T, typename enable = void>
+struct is_dynamic_vector_wrapper_eigen : std::false_type {};
+
+template <typename T>
+struct is_dynamic_vector_wrapper_eigen<
+  T, ::pressio::mpl::enable_if_t<
+       containers::details::traits<T>::is_vector &&
+       (containers::details::traits<T>::wrapped_vector_identifier==
+        containers::details::WrappedVectorIdentifier::EigenColDynamic or
+        containers::details::traits<T>::wrapped_vector_identifier==
+        containers::details::WrappedVectorIdentifier::EigenRowDynamic)
+       >
+  > : std::true_type{};
+
+
 }}}//end namespace pressio::containers::meta
 #endif
