927a20b5470c926762c7ec252cfab1db0e6eb9e2
Eric Joshua Parish <ejparis@sandia.gov>
2020-01-14 16:33:31 -0800

    removed outdated single_step_policies directory, this is replaced by ode

--- packages/rom/src/wls/policies/single_step_policies/wls_jacobian_policies_singlestep.hpp
+++
@@ -1,4 +0,0 @@
-#include "./impl_singlestep/wls_jacobian_policies_singlestep_impl.hpp"
-namespace pressio{ namespace rom{ namespace wls{ 
-using local_jacobian_policy_velocityAPI = ::pressio::rom::wls::impl::local_jacobian_policy_velocityAPI;
-}}}
--- packages/rom/src/wls/policies/single_step_policies/wls_residual_policies_singlestep.hpp
+++
@@ -1,4 +0,0 @@
-#include "./impl_singlestep/wls_residual_policies_singlestep_impl.hpp"
-namespace pressio{ namespace rom{ namespace wls{ 
-using local_residual_policy_velocityAPI = ::pressio::rom::wls::impl::local_residual_policy_velocityAPI;//<fom_type,fom_state_type,residual_type,aux_states_container_type,scalar_type>;
-}}}
