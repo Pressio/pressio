7fe151a629f3d50d0d4766d5f97c72ecb9db9917
Francesco Rizzi <fnrizzi@sandia.gov>
2020-03-30 10:11:27 +0200

    tests: add rosenbrock test for optimizers

--- tests/optimizers/eigen_rosenbrock/main.cc
+++ tests/optimizers/eigen_rosenbrock/main.cc
@@ -36,17 +36,25 @@ int main(int argc, char *argv[])
 
   // Set Initial Guess
   state_t x(2);
-  x[0] = -3.;
-  x[1] = -4.;
+  x[0] = -3.; x[1] = -4.;
 
   using opt_param_t = pressio::optimizers::Parameters<sc_t>;
   opt_param_t MyPars;
-  // set parameters
+  MyPars.setGradientNormOptimalityTolerance(1e-8);
+  MyPars.setStepNormOptimalityTolerance(1e-2);
 
   using opt_prob_t  = pressio::optimizers::Unconstrained<obj_t>;
   opt_prob_t optProblem(MyPars);
   optProblem.solve(rosenbrock, x);
 
-  std::cout << x[0] << " " << x[1] << std::endl;
+  std::cout << std::setprecision(15) << x[0] << " " << x[1] << std::endl;
+
+  const auto err1 = std::abs(0.999994936086583 - x[0]);
+  const auto err2 = std::abs(0.999989789232424 - x[1]);
+  if (err1 < 1e-12 and err2 < 1e-12)
+    std::cout << "PASSED" << std::endl;
+  else
+    std::cout << "FAILED" << std::endl;
+
   return 0;
 }
