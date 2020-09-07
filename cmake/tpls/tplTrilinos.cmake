c461ab29b3ce8f82a1aad2836524dbf9fceedca0
Francesco Rizzi <fnrizzi@sandia.gov>
2020-09-07 10:50:15 +0200

    cmake: working on #182

--- cmake/tpls/tplTrilinos.cmake
+++ cmake/tpls/tplTrilinos.cmake
@@ -1,8 +1,17 @@
 
-option(PRESSIO_ENABLE_TPL_TRILINOS "Enable Trilinos TPL" OFF)
+
+# if trilinos is on, then also set MPI, BLAS, LAPACK and KOKKOS ON
+if(PRESSIO_ENABLE_TPL_TRILINOS)
+  message("> PRESSIO_ENABLE_TPL_TRILINOS=ON ==> enabling also BLAS, LAPACK, MPI, KOKKOS")
+
+  set(PRESSIO_ENABLE_TPL_KOKKOS ON)
+  set(PRESSIO_ENABLE_TPL_MPI ON)
+  set(PRESSIO_ENABLE_TPL_BLAS ON)
+  set(PRESSIO_ENABLE_TPL_LAPACK ON)
+endif()
 
 if(PRESSIO_ENABLE_TPL_TRILINOS)
-  message("Enabling Trilinos since PRESSIO_ENABLE_TPL_TRILINOS=${PRESSIO_ENABLE_TPL_TRILINOS}")
+  #message("Enabling Trilinos since PRESSIO_ENABLE_TPL_TRILINOS=${PRESSIO_ENABLE_TPL_TRILINOS}")
 
   # if we need to build tests, then find trilinos
   if(PRESSIO_ENABLE_UNIT_TESTS OR PRESSIO_ENABLE_TESTS)
