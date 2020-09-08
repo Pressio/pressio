0bd7e9034c0eebdcd13856ee5c668089841e806d
Francesco Rizzi <fnrizzi@sandia.gov>
2020-09-08 11:54:26 +0200

    rom:tests: clean test file a bit

--- tests/rom/burgers1d/lspg/eigen/lspg_euler_normal_eq/main.cc
+++ tests/rom/burgers1d/lspg/eigen/lspg_euler_normal_eq/main.cc
@@ -3,42 +3,46 @@
 #include "pressio_apps.hpp"
 #include "utils_eigen.hpp"
 
-int main(int argc, char *argv[]){
-  using fom_t		= pressio::apps::Burgers1dEigen;
-  using scalar_t	= typename fom_t::scalar_type;
-  using native_state_t  = typename fom_t::state_type;
-  using fom_state_t  = pressio::containers::Vector<native_state_t>;
-
-  using eig_dyn_vec	= Eigen::Matrix<scalar_t, -1, 1>;
-  using lspg_state_t	= pressio::containers::Vector<eig_dyn_vec>;
-
-  using eig_dyn_mat	= Eigen::Matrix<scalar_t, -1, -1>;
-  using decoder_jac_t	= pressio::containers::MultiVector<eig_dyn_mat>;
-  using decoder_t	= pressio::rom::LinearDecoder<decoder_jac_t, fom_state_t>;
-
+int main(int argc, char *argv[])
+{
   std::string checkStr {"PASSED"};
 
-  //-------------------------------
-  // app object
+  using fom_t		= pressio::apps::Burgers1dEigen;
+  using scalar_t	= typename fom_t::scalar_type;
+
+  // -------------------------------------------------------
+  // create FOM object
+  // -------------------------------------------------------
   constexpr int numCell = 20;
   Eigen::Vector3d mu(5.0, 0.02, 0.02);
   fom_t appobj( mu, numCell);
-  auto t0 = static_cast<scalar_t>(0);
-  scalar_t dt = 0.01;
-
-  // read from file the jacobian of the decoder
-  constexpr int romSize = 11;
-  // store modes computed before from file
-  decoder_jac_t phi =
-    pressio::rom::test::eigen::readBasis("basis.txt", romSize, numCell);
-  const int numBasis = phi.numVectors();
-  if( numBasis != romSize ) return 0;
-
-  // create decoder obj
-  decoder_t decoderObj(phi);
 
   // for this problem, my reference state = initial state
   auto & yRef = appobj.getInitialState();
+
+  // -------------------------------------------------------
+  // read basis
+  // -------------------------------------------------------
+  using native_dense_matrix_t = typename fom_t::dense_matrix_type;
+  using decoder_jac_t	= pressio::containers::MultiVector<native_dense_matrix_t>;
+  constexpr int romSize = 11;
+  decoder_jac_t phi = pressio::rom::test::eigen::readBasis("basis.txt", romSize, numCell);
+  const int numBasis = phi.numVectors();
+  if( numBasis != romSize ) return 0;
+
+  // -------------------------------------------------------
+  // create decoder obj
+  // -------------------------------------------------------
+  using native_state_t  = typename fom_t::state_type;
+  using fom_state_t  = pressio::containers::Vector<native_state_t>;
+  using decoder_t = pressio::rom::LinearDecoder<decoder_jac_t, fom_state_t>;
+  decoder_t decoderObj(phi);
+
+
+  // -------------------------------------------------------
+  // create ROM problem
+  // -------------------------------------------------------
+  using lspg_state_t = pressio::containers::Vector<Eigen::Matrix<scalar_t,-1,1>>;
 
   // define ROM state
   lspg_state_t yROM(romSize);
@@ -47,7 +51,8 @@ int main(int argc, char *argv[]){
 
   // define LSPG type
   using ode_tag  = pressio::ode::implicitmethods::Euler;
-  using lspg_problem = typename pressio::rom::lspg::composeDefaultProblem<ode_tag, fom_t, lspg_state_t, decoder_t>::type;
+  using lspg_problem = typename pressio::rom::lspg::composeDefaultProblem
+    <ode_tag, fom_t, lspg_state_t, decoder_t>::type;
   using lspg_stepper_t = typename lspg_problem::lspg_stepper_t;
   lspg_problem lspgProblem(appobj, yRef, decoderObj, yROM);
 
@@ -69,6 +74,7 @@ int main(int argc, char *argv[]){
   solver.setMaxIterations(4);
 
   // integrate in time
+  scalar_t dt = 0.01;
   pressio::ode::advanceNSteps(lspgProblem.getStepperRef(), yROM, 0.0, dt, 10, solver);
 
   // compute the fom corresponding to our rom final state
