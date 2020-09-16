005a45939efc8708ae33b9d89fd08d175621360c
Francesco Rizzi <fnrizzi@sandia.gov>
2020-09-16 20:46:38 +0200

    ode: forgot one case for 3 total states

--- packages/ode/src/predicates/discrete_time_residual_methods/ode_has_const_discrete_time_residual_method_accept_step_time_dt_result_norm_states_return_void.hpp
+++ packages/ode/src/predicates/discrete_time_residual_methods/ode_has_const_discrete_time_residual_method_accept_step_time_dt_result_norm_states_return_void.hpp
@@ -100,5 +100,28 @@ struct has_const_discrete_time_residual_method_accept_step_time_dt_result_norm_n
     >
   > : std::true_type{};
 
+
+template <typename T, typename step_t, typename sc_t, typename state_t, typename result_t>
+struct has_const_discrete_time_residual_method_accept_step_time_dt_result_norm_n_states_return_void<
+  T, 3, step_t, sc_t, state_t, result_t,
+  ::pressio::mpl::enable_if_t<
+    std::is_void<
+      decltype(
+	       std::declval<T const>().discreteTimeResidual(
+							    std::declval<step_t const &>(),
+							    std::declval<sc_t const &>(),
+							    std::declval<sc_t const &>(),
+							    std::declval<result_t &>(),
+							    ::pressio::Norm::Undefined,
+							    std::declval<sc_t &>(),
+							    std::declval<state_t const&>(),
+							    std::declval<state_t const&>(),
+							    std::declval<state_t const&>()
+							    )
+	       )
+      >::value
+    >
+  > : std::true_type{};
+
 }}} 
 #endif  // ODE_PREDICATES_DISCRETE_TIME_RESIDUAL_METHODS_ODE_HAS_CONST_DISCRETE_TIME_RESIDUAL_METHOD_ACCEPT_STEP_TIME_DT_RESULT_NORM_STATES_RETURN_VOID_HPP_
