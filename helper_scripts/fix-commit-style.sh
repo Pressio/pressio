70c1f7b8142d5aa7ac7b54bb7ba07b2c61e7eeb5
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2021-07-30 20:14:30 +0200

    reorganize auto-formatting scripts #116

--- helper_scripts/fix-commit-style.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-#
-# Run this script to fix styling in your last commit (changes only selected lines)
-#
-# Note: need clang-format-diff.py in path (e.g. /usr/share/clang/clang-format-10)
-
-git diff -U0 --no-color HEAD^ | clang-format-diff.py -i -p1
--- helper_scripts/fix-style.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-#
-# Run this script to fix styling with clang-format in all source files
-#
-
-find . -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp \
-    | xargs clang-format -style=file -i -fallback-style=none
