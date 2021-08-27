2cb3a20a04ae4da1b9949405d8919c1c901143e2
Eric Joshua Parish <ejparis@sandia.gov>
2021-08-26 18:13:22 -0700

    pushed main tpql code

---
+++ include/pressio/rom/tpw_roms/tpql_main.hpp
@@ -0,0 +1,228 @@
+#ifndef TPQL_MAIN_HPP_
+#define TPQL_MAIN_HPP_
+
+#include "tpql_operators.hpp"
+#include "pressio/ops.hpp"
+
+namespace pressio{ namespace rom{ namespace experimental{
+
+template <typename app_t, typename basis_t, typename scalar_t>
+class TpwqRom
+{
+public:
+  template <typename list_of_states_t, typename list_of_params_t, 
+            typename list_of_times_t, typename list_of_coords_t, typename fom_state_t> 
+  TpwqRom(const app_t & appObj,const basis_t & Phi,const list_of_states_t & InputListOfStatesAtLinearizationPoints,
+          const list_of_params_t & InputListOfParamsAtLinearizationPoints, const list_of_times_t & InputListOfTimesAtLinearizationPoints, 
+          const list_of_coords_t & InputListOfCoordsAtLinearizationPoints, const fom_state_t & fomReferenceState, const scalar_t & velocityErrorTolerance,
+          const scalar_t FiniteDifferenceStepSize) : Phi_(Phi) , appObj_(appObj){
+
+
+  // Compute mean of coordinates
+  int numberOfInputPoints = InputListOfCoordsAtLinearizationPoints.size(); 
+  int coordinateDimension = InputListOfCoordsAtLinearizationPoints[0].size();
+
+  std::vector<scalar_t> coordinateMeans(coordinateDimension);
+  for (int i = 0; i < numberOfInputPoints; i++){
+    for (int j=0; j < coordinateDimension; j++){ 
+      coordinateMeans[j] += InputListOfCoordsAtLinearizationPoints[i](j);
+    }
+  }
+
+  for (int j=0; j < coordinateDimension; j++){ 
+   coordinateMeans[j] /= float(numberOfInputPoints);
+  }
+
+  std::vector<scalar_t> distanceFromMean(numberOfInputPoints);
+  for (int i = 0; i < numberOfInputPoints; i++){
+    for (int j=0; j < coordinateDimension; j++){ 
+      distanceFromMean[i] += std::pow( InputListOfCoordsAtLinearizationPoints[i](j) - coordinateMeans[j] ,2.);
+    }
+    distanceFromMean[i] = std::sqrt(distanceFromMean[i]);
+  }
+
+  std::vector<int> sortedIndices(numberOfInputPoints);
+  for (int i=0;i<numberOfInputPoints;i++){sortedIndices[i] = i;};
+  std::sort( sortedIndices.begin(),sortedIndices.end(), [&](int i,int j){return distanceFromMean[i]<distanceFromMean[j];} );
+ 
+
+  auto workingStateVector = appObj.createVelocity(); 
+  for (int i = 0; i < numberOfInputPoints; i++){
+    // Construct state at current point
+    int sortedIndex = sortedIndices[i];
+    auto currentState = InputListOfStatesAtLinearizationPoints[sortedIndex];
+    auto currentTime = InputListOfTimesAtLinearizationPoints[sortedIndex];
+    auto currentParam = InputListOfParamsAtLinearizationPoints[sortedIndex];
+    auto currentCoords = InputListOfCoordsAtLinearizationPoints[sortedIndex];
+
+    ::pressio::ops::product(::pressio::nontranspose(), 1., Phi,currentState,0.,workingStateVector);
+    ::pressio::ops::update(workingStateVector,1.,fomReferenceState,1.); 
+    if (i == 0){
+      // Add point to coordinate list
+      ListOfStatesAtLinearizationPoints_.push_back( currentState );  
+      ListOfParamsAtLinearizationPoints_.push_back( currentParam );  
+      ListOfTimesAtLinearizationPoints_.push_back( currentTime );  
+      ListOfCoordsAtLinearizationPoints_.push_back( currentCoords );  
+
+
+      auto PhiTf = ::pressio::rom::experimental::computeBasisTransposeTimesVelocity(appObj, workingStateVector,currentParam,currentTime, Phi);
+      ListOfVelocitiesAtLinearizationPoints_.push_back(PhiTf);
+
+      auto PhiTJPhi = ::pressio::rom::experimental::computeBasisTransposeTimesJacobianTimesBasis(appObj,workingStateVector,currentParam,currentTime, Phi);
+      ListOfJacobiansAtLinearizationPoints_.push_back(PhiTJPhi);
+
+      auto PhiTHPhiPhi = ::pressio::rom::experimental::computeBasisTransposeTimesHessianTimesBasisTimesBasis(appObj,workingStateVector,currentParam,currentTime, Phi);
+      ListOfHessiansAtLinearizationPoints_.push_back(PhiTHPhiPhi);
+
+      auto PhiTJParams = ::pressio::rom::experimental::computeBasisTransposeTimesParameterJacobian(appObj,workingStateVector,currentParam,currentTime, Phi);
+      ListOfParamJacobiansAtLinearizationPoints_.push_back(PhiTJParams);
+
+      auto PhiTHParams = ::pressio::rom::experimental::computeBasisTransposeTimesParameterHessian(appObj,workingStateVector,currentParam,currentTime, Phi);
+      ListOfParamHessiansAtLinearizationPoints_.push_back(PhiTHParams);
+
+      auto PhiTHMixed = ::pressio::rom::experimental::computeBasisTransposeTimesMixedHessian(appObj,workingStateVector,currentParam,currentTime, Phi);
+      ListOfMixedHessiansAtLinearizationPoints_.push_back(PhiTHMixed);
+      
+    }
+    else{
+
+      auto relativeError = CheckGalerkinVelocity(currentState,workingStateVector,currentParam,currentTime,currentCoords);
+
+      if (relativeError >= velocityErrorTolerance){
+        std::cout << " adding new point " << std::endl; 
+        // Add point to coordinate list
+        ListOfStatesAtLinearizationPoints_.push_back( currentState );  
+        ListOfParamsAtLinearizationPoints_.push_back( currentParam );  
+        ListOfTimesAtLinearizationPoints_.push_back( currentTime );  
+        ListOfCoordsAtLinearizationPoints_.push_back( currentCoords );  
+  
+
+        auto PhiTf = ::pressio::rom::experimental::computeBasisTransposeTimesVelocity(appObj, workingStateVector,currentParam,currentTime, Phi);
+        ListOfVelocitiesAtLinearizationPoints_.push_back(PhiTf);
+  
+        auto PhiTJPhi = ::pressio::rom::experimental::computeBasisTransposeTimesJacobianTimesBasis(appObj,workingStateVector,currentParam,currentTime, Phi);
+        ListOfJacobiansAtLinearizationPoints_.push_back(PhiTJPhi);
+  
+        auto PhiTHPhiPhi = ::pressio::rom::experimental::computeBasisTransposeTimesHessianTimesBasisTimesBasis(appObj,workingStateVector,currentParam,currentTime, Phi);
+        ListOfHessiansAtLinearizationPoints_.push_back(PhiTHPhiPhi);
+  
+        auto PhiTJParams = ::pressio::rom::experimental::computeBasisTransposeTimesParameterJacobian(appObj,workingStateVector,currentParam,currentTime, Phi);
+        ListOfParamJacobiansAtLinearizationPoints_.push_back(PhiTJParams);
+  
+        auto PhiTHParams = ::pressio::rom::experimental::computeBasisTransposeTimesParameterHessian(appObj,workingStateVector,currentParam,currentTime, Phi);
+        ListOfParamHessiansAtLinearizationPoints_.push_back(PhiTHParams);
+  
+        auto PhiTHMixed = ::pressio::rom::experimental::computeBasisTransposeTimesMixedHessian(appObj,workingStateVector,currentParam,currentTime, Phi);
+        ListOfMixedHessiansAtLinearizationPoints_.push_back(PhiTHMixed);
+  
+      }
+    }
+  } 
+ 
+  }
+private:
+  // types
+  using rom_state_t = Eigen::Matrix<scalar_t,-1,1>;
+  using params_t = Eigen::Matrix<scalar_t,-1,1>;
+  using coords_t = Eigen::Matrix<scalar_t,-1,1>;
+
+  using rom_velocity_t = Eigen::Matrix<scalar_t,-1,1>;
+  using rom_jacobian_t = Eigen::Matrix<scalar_t,-1,-1>;
+  using rom_hessian_t = std::vector< std::vector< std::vector< scalar_t> > >; 
+
+  using list_of_states_t = std::vector<rom_state_t>;
+  using list_of_params_t = std::vector<params_t>;
+  using list_of_times_t = std::vector<scalar_t>;
+  using list_of_coords_t = std::vector<coords_t>;
+
+  const app_t & appObj_;
+  const basis_t & Phi_;
+
+  list_of_states_t ListOfStatesAtLinearizationPoints_{0};
+  list_of_params_t ListOfParamsAtLinearizationPoints_{0}; 
+  list_of_times_t  ListOfTimesAtLinearizationPoints_{0};
+  list_of_coords_t  ListOfCoordsAtLinearizationPoints_{0};
+
+  std::vector< rom_velocity_t > ListOfVelocitiesAtLinearizationPoints_{0};
+  std::vector< rom_jacobian_t > ListOfJacobiansAtLinearizationPoints_{0};
+  std::vector< rom_hessian_t  > ListOfHessiansAtLinearizationPoints_{0}; 
+  std::vector< rom_jacobian_t  > ListOfParamJacobiansAtLinearizationPoints_{0}; 
+  std::vector< rom_hessian_t  > ListOfParamHessiansAtLinearizationPoints_{0}; 
+  std::vector< rom_hessian_t  > ListOfMixedHessiansAtLinearizationPoints_{0}; 
+
+  template <typename params_t, typename coords_t>
+  rom_state_t GalerkinVelocity(const rom_state_t & xhat,const params_t & mu,const scalar_t t,const coords_t & PointCoords){
+    int linPointIndx = FindClosestLinearizationPoint(PointCoords);
+    auto VelocityAtLinearizationPoint = ListOfVelocitiesAtLinearizationPoints_[linPointIndx];
+    auto JacobianAtLinearizationPoint = ListOfJacobiansAtLinearizationPoints_[linPointIndx];
+    auto HessianAtLinearizationPoint =  ListOfHessiansAtLinearizationPoints_[linPointIndx];
+    auto ParameterJacobianAtLinearizationPoint = ListOfParamJacobiansAtLinearizationPoints_[linPointIndx];
+    auto ParameterHessianAtLinearizationPoint = ListOfParamHessiansAtLinearizationPoints_[linPointIndx];
+    auto MixedHessianAtLinearizationPoint = ListOfMixedHessiansAtLinearizationPoints_[linPointIndx];
+    auto ReducedStateAtLinearizationPoint = ListOfStatesAtLinearizationPoints_[linPointIndx];
+    auto ParamAtLinearizationPoint = ListOfParamsAtLinearizationPoints_[linPointIndx];
+
+    auto d_xHat = xhat - ReducedStateAtLinearizationPoint;
+    auto d_mu = mu - ParamAtLinearizationPoint;
+    auto f = VelocityAtLinearizationPoint + JacobianAtLinearizationPoint * d_xHat + 
+             ParameterJacobianAtLinearizationPoint * d_mu;
+    tensorMultiply(HessianAtLinearizationPoint, d_xHat,d_xHat,0.5,f); 
+    tensorMultiply(ParameterHessianAtLinearizationPoint, d_mu,d_mu,0.5,f); 
+    tensorMultiply(MixedHessianAtLinearizationPoint, d_xHat,d_mu,1.,f); 
+    return f;
+  }
+
+ 
+  void tensorMultiply(rom_hessian_t H, rom_state_t a1, rom_state_t a2,scalar_t alpha, rom_velocity_t f){
+    auto d1 = H.size();
+    auto d2 = H[0].size();
+    auto d3 = H[0][0].size();
+    for (int i = 0; i < d1; i++){
+      for (int j = 0; j < d2; j++){
+        for (int k = 0; k < d3; k++){
+          f[i] += alpha*H[i][j][k] * a1(j) * a2(k);
+        }
+      }
+    } 
+  }
+
+  template <typename fom_state_t, typename params_t, typename coords_t>
+  double CheckGalerkinVelocity(const rom_state_t & xhat,const fom_state_t & x, const params_t & mu,const scalar_t t,const coords_t & PointCoords)
+  {
+    auto f = GalerkinVelocity(xhat,mu,t,PointCoords);
+    auto fTrue = appObj_.createVelocity();
+    //appObj.updateParams(mu);
+    appObj_.velocity(x,t,fTrue);
+
+    rom_velocity_t fTrueProject( xhat.size());
+    ::pressio::ops::product(::pressio::transpose(),1., Phi_,fTrue, 0.,fTrueProject);    
+    auto error = fTrue - fTrueProject;
+    auto errorNorm = error.norm(); 
+    return errorNorm;
+  }
+
+  template<typename coords_t>
+  int FindClosestLinearizationPoint(coords_t PointCoords){
+
+    int numberOfAddedLinearizationPoints = ListOfCoordsAtLinearizationPoints_.size();
+    int coordinateDimension = 2;
+    std::vector<scalar_t> distance(numberOfAddedLinearizationPoints);
+    std::cout << "Current coords are " << PointCoords << std::endl;
+
+    for (int i = 0; i < numberOfAddedLinearizationPoints; i++){
+      for (int j=0; j < coordinateDimension; j++){
+        distance[i] += std::pow( ListOfCoordsAtLinearizationPoints_[i](j) - PointCoords[j] ,2.);
+      }
+      distance[i] = std::sqrt(distance[i]);
+    }
+    int linPointIndex = std::min_element(distance.begin(), distance.end()) - distance.begin();
+    std::cout << "Closest linearization point is " << linPointIndex << std::endl;
+    return linPointIndex;
+  }
+
+  //template <typename params_t, typename coords_t>
+  //void GalerkinVelocity(const rom_state_t & xhat,const params_t & mu,const scalar_t t,const coords_t & PointCoords){
+  //}
+};
+}}}
+#endif
---
+++ include/pressio/rom/tpw_roms/tpql_operators.hpp
@@ -0,0 +1,412 @@
+#include "pressio/ops.hpp"
+
+#ifndef TPQL_OPS_HPP_
+#define TPQL_OPS_HPP_
+
+namespace pressio{ namespace rom{ namespace experimental{
+
+template <typename scalar_t, typename matrix_t>
+Eigen::Matrix<scalar_t, -1 , 1> getCol(const matrix_t & Phi, int i){
+  Eigen::Matrix<scalar_t,-1,1> PhiCol = Phi.col(i);
+  return PhiCol;
+}
+
+template <typename matrix_t, typename vec_t>
+void setCol(matrix_t & Phi,const vec_t & PhiCol, int i){
+  Phi.col(i) = PhiCol;
+}
+
+
+
+
+template <typename app_t, typename state_t, typename param_t , typename scalar_t, typename basis_t>
+std::vector< std::vector< std::vector< scalar_t > > > computeBasisTransposeTimesMixedHessian(const app_t & appObj,const state_t & y,const param_t & mu,const  scalar_t t,const basis_t & Phi)
+{
+  auto f0 = appObj.createVelocity();
+  appObj.velocity( y , t , f0);
+  auto ftmp = appObj.createVelocity();
+  auto workingVector = appObj.createVelocity();
+
+  auto romDim = ::pressio::ops::extent(Phi,1);
+  auto fomDim = ::pressio::ops::extent(y,0);
+  auto numParams = ::pressio::ops::extent(mu,0);
+  scalar_t eps = 1e-3;
+  auto yp = ::pressio::ops::clone(y);
+  auto muP = ::pressio::ops::clone(mu);
+
+  std::vector< std::vector< std::vector< scalar_t > > > BasisTransposeTimesMixedHessian(romDim);
+  Eigen::Matrix<scalar_t,-1,1> BasisTransposeTimesMixedHessianCol(romDim);
+
+  for (int i=0; i < romDim; i++){
+    BasisTransposeTimesMixedHessian[i].resize(romDim);
+  }
+  for (int i=0; i < romDim; i++){
+    for (int j=0; j < romDim; j++){
+      BasisTransposeTimesMixedHessian[i][j].resize(numParams);
+    }
+  }
+  for  (int i = 0; i < romDim; i++){
+    auto PhiCol = getCol<scalar_t>(Phi,i);
+    for (int j=0; j < numParams; j++){
+      ::pressio::ops::set_zero(workingVector);
+      ::pressio::ops::update(yp,0.,y,1.,PhiCol,eps);
+      muP(j) += eps;
+      //appObj.updateParams(muP);
+      ::pressio::ops::set_zero(ftmp);
+      appObj.velocity(yp,t,ftmp);
+      ::pressio::ops::update(workingVector,0.,ftmp,1.);
+      muP(j) -= eps;
+
+      muP(j) -= eps;
+      //appObj.updateParams(muP);
+      ::pressio::ops::set_zero(ftmp);
+      appObj.velocity(yp,t,ftmp);
+      workingVector -= ftmp;
+      ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+      muP(j) += eps;
+
+      yp = y - eps*PhiCol;
+      ::pressio::ops::update(yp,1.,PhiCol,-2.*eps);
+      muP(j) += eps;
+      //appOBj.updateParams(muP)
+      ::pressio::ops::set_zero(ftmp);
+      appObj.velocity(yp,t,ftmp);
+      ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+      muP(j) -= eps;
+
+      muP(j) -= eps;
+      //appObj.udpateParams(muP);
+      ::pressio::ops::set_zero(ftmp);
+      appObj.velocity(yp,t,ftmp);
+      ::pressio::ops::update(workingVector,1.,ftmp,1.);
+      muP(j) += eps;
+
+      ::pressio::ops::scale(workingVector,0.25/(eps*eps));
+
+      ::pressio::ops::product(::pressio::transpose(),1., Phi,workingVector , 0.,BasisTransposeTimesMixedHessianCol);
+      for (int k = 0; k < romDim; k++){
+        BasisTransposeTimesMixedHessian[k][i][j] = BasisTransposeTimesMixedHessianCol(k);
+      } 
+    }
+  }
+  return BasisTransposeTimesMixedHessian; 
+}
+
+
+template <typename app_t, typename state_t, typename param_t , typename scalar_t, typename basis_t>
+std::vector< std::vector< std::vector< scalar_t > > > computeBasisTransposeTimesParameterHessian(const app_t & appObj,const state_t & y,const param_t & mu,const  scalar_t t,const basis_t & Phi)
+{
+  auto f0 = appObj.createVelocity();
+  appObj.velocity( y , t , f0);
+  auto ftmp = appObj.createVelocity();
+  auto workingVector = appObj.createVelocity();
+
+  auto romDim = ::pressio::ops::extent(Phi,1);
+  auto fomDim = ::pressio::ops::extent(y,0);
+  auto numParams = ::pressio::ops::extent(mu,0);
+  scalar_t eps = 1e-3;
+
+  auto muP = ::pressio::ops::clone(mu);
+
+  std::vector< std::vector< std::vector< scalar_t > > > BasisTransposeTimesParameterHessian(romDim);
+  Eigen::Matrix<scalar_t , -1,1> BasisTransposeTimesParameterHessianCol(romDim);
+  for (int i=0; i < romDim; i++){
+    BasisTransposeTimesParameterHessian[i].resize(numParams);
+  }
+  for (int i=0; i < romDim; i++){
+    for (int j=0; j < numParams; j++){
+      BasisTransposeTimesParameterHessian[i][j].resize(numParams);
+    }
+  }
+  for  (int i = 0; i < numParams; i++){
+    for (int j=0; j < numParams; j++){
+      ::pressio::ops::set_zero(workingVector);
+      if (i == j){
+        muP(i) += 2.*eps;
+        //appObj.updateParams(muP);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,0.,ftmp,-1.);
+        muP(i) -= 2.*eps;
+
+        muP(i) += eps;
+        //appObj.updateParams(muP);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,16.);
+        muP(i) -= eps;
+
+        ::pressio::ops::update(workingVector,1.,f0,-30.);
+         
+        muP(i) -= eps;
+        //appObj.updateParams(muP);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,16.);
+        muP(i) += eps;
+
+        muP(i) -= 2.*eps; 
+        //appObj.updateParams(muP);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        workingVector -= ftmp;
+        ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+        muP(i) += 2.*eps;
+        ::pressio::ops::scale(workingVector, 1./(12.*(eps*eps)));
+      }
+      else{
+        muP(i) += eps;
+        muP(j) += eps;
+        //appObj.updateParams(muP);
+        ftmp.setZero();
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,0.,ftmp,1.);
+        muP(i) -= eps;
+        muP(j) -= eps;
+
+        muP(i) += eps;
+        muP(j) -= eps;
+        //appObj.updateParams(muP);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+        muP(i) -= eps;
+        muP(j) += eps;
+
+        muP(i) -= eps;
+        muP(j) += eps;
+        //appOBj.updateParams(muP)
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+        muP(i) += eps;
+        muP(j) -= eps;
+
+        muP(i) -= eps;
+        muP(j) -= eps;
+        //appObj.udpateParams(muP);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(y,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,1.);
+        muP(i) += eps;
+        muP(j) += eps;
+
+        ::pressio::ops::scale(workingVector,0.25/(eps*eps));
+
+      }
+      ::pressio::ops::product(::pressio::transpose(),1., Phi,workingVector , 0.,BasisTransposeTimesParameterHessianCol);
+
+      for (int k = 0; k < romDim; k++){
+        BasisTransposeTimesParameterHessian[k][i][j] = BasisTransposeTimesParameterHessianCol(k);
+      } 
+    }
+  }
+  return BasisTransposeTimesParameterHessian; 
+}
+
+
+
+template <typename app_t, typename state_t, typename param_t , typename scalar_t, typename basis_t>
+Eigen::MatrixXd computeBasisTransposeTimesParameterJacobian(const app_t & appObj,const state_t & y,const param_t & mu,const scalar_t t,const basis_t & Phi)
+{
+  auto ftmp = appObj.createVelocity();
+  auto workingVector = appObj.createVelocity();
+
+  auto numParams = ::pressio::ops::extent(mu,0);
+  auto fomDim = ::pressio::ops::extent(y,0);
+  auto romDim = ::pressio::ops::extent(Phi,1);
+
+  scalar_t eps = 1e-3;
+  Eigen::Matrix<scalar_t, -1, -1> PhiTJPhi(romDim,numParams);
+  Eigen::Matrix<scalar_t, -1, 1> PhiTJPhiCol(romDim);
+
+  for  (int i = 0; i < numParams; i++){
+    ::pressio::ops::set_zero(workingVector);
+
+    Eigen::Matrix<scalar_t, -1, 1>  muP = mu;
+    muP(i) += eps;
+    //appObj.updateParams(muP); 
+    ::pressio::ops::set_zero(ftmp);
+    appObj.velocity(y,t,ftmp);
+    ::pressio::ops::update(workingVector,0.,ftmp,1.);
+
+    muP(i) -= 2.*eps;
+    ::pressio::ops::set_zero(ftmp);
+    //appObj.updateParams(muP); 
+    appObj.velocity(y,t,ftmp);
+    ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+
+    ::pressio::ops::scale(workingVector, 0.5/eps);
+
+    PhiTJPhi.col(i) = Phi.transpose() * workingVector;
+    ::pressio::ops::product(::pressio::transpose(),1., Phi,workingVector , 0., PhiTJPhiCol);
+    setCol(PhiTJPhi,PhiTJPhiCol,i);
+  
+  }
+  return PhiTJPhi;
+} 
+
+
+template <typename app_t, typename state_t, typename param_t , typename scalar_t, typename basis_t>
+Eigen::Matrix<scalar_t,-1,1> computeBasisTransposeTimesVelocity(const app_t & appObj, const state_t & y,const param_t & mu,const scalar_t t,const  basis_t & Phi)
+{
+  auto romDim = ::pressio::ops::extent(Phi,1);
+  auto fomDim = ::pressio::ops::extent(y,0);
+
+  // Create temporary working vectors
+  auto f = appObj.createVelocity();
+  appObj.velocity(y,t,f);
+  Eigen::Matrix<scalar_t, -1, 1> PhiTfCol(romDim);
+
+  ::pressio::ops::product(::pressio::transpose(),1., Phi,f, 0.,PhiTfCol);
+  return PhiTfCol;
+} 
+
+
+
+template <typename app_t, typename state_t, typename param_t , typename scalar_t, typename basis_t>
+Eigen::MatrixXd computeBasisTransposeTimesJacobianTimesBasis(const app_t & appObj, const state_t & y,const param_t & mu,const scalar_t t,const  basis_t & Phi)
+{
+  auto romDim = ::pressio::ops::extent(Phi,1);
+  auto fomDim = ::pressio::ops::extent(y,0);
+
+  // Create temporary working vectors
+  auto fp = appObj.createVelocity();
+  auto yp = ::pressio::ops::clone(y);
+  auto workingVector = appObj.createVelocity();
+
+  // Finite difference step size
+  scalar_t eps = 1e-3;
+
+  // Create result
+  Eigen::Matrix<scalar_t, -1, -1> PhiTJPhi(romDim,romDim);
+  Eigen::Matrix<scalar_t, -1, 1> PhiTJPhiCol(romDim);
+
+
+  // Finite difference loop
+  for  (int i = 0; i < romDim; i++){
+    ::pressio::ops::set_zero(workingVector);
+
+    auto PhiCol = getCol<scalar_t>(Phi,i);
+    ::pressio::ops::update(yp,0.,y,1.,PhiCol,eps);
+    appObj.velocity(yp,t,fp);
+    ::pressio::ops::update(workingVector,0.,fp,1.);
+
+    ::pressio::ops::update(yp,1.,PhiCol,-2.*eps);
+    appObj.velocity(yp,t,fp);
+    ::pressio::ops::update(workingVector,1.,fp,-1.);
+
+    ::pressio::ops::scale(workingVector,0.5/eps); 
+ 
+    ::pressio::ops::product(::pressio::transpose(),1., Phi,workingVector , 0., PhiTJPhiCol);
+    setCol(PhiTJPhi,PhiTJPhiCol,i);
+  }
+  return PhiTJPhi;
+} 
+  
+template <typename app_t, typename state_t, typename param_t , typename scalar_t, typename basis_t>
+std::vector< std::vector< std::vector< scalar_t > > > computeBasisTransposeTimesHessianTimesBasisTimesBasis(const app_t & appObj,const state_t & y,const param_t & mu,const scalar_t t,const basis_t & Phi)
+{
+  auto romDim = Phi.cols();
+  auto fomDim = y.size();
+
+  // Create temporary working vectors
+  auto f0 = appObj.createVelocity();
+  appObj.velocity( y , t , f0);
+  auto ftmp = appObj.createVelocity();
+  auto workingVector = appObj.createVelocity();
+
+  scalar_t eps = 1e-3;
+  auto yp = ::pressio::ops::clone(y); //pressio ops clone 
+
+  std::vector< std::vector< std::vector< scalar_t > > > BasisTransposeTimesHessianTimesBasisTimesBasis(romDim);
+  Eigen::Matrix<scalar_t, -1, 1> HCol(romDim);
+
+  for (int i=0; i < romDim; i++){
+    BasisTransposeTimesHessianTimesBasisTimesBasis[i].resize(romDim);
+  }
+  for (int i=0; i < romDim; i++){
+    for (int j=0; j < romDim; j++){
+      BasisTransposeTimesHessianTimesBasisTimesBasis[i][j].resize(romDim);
+    }
+  }
+
+  for  (int i = 0; i < romDim; i++){
+    for (int j=0; j < romDim; j++){
+      ::pressio::ops::set_zero(workingVector);
+      if (i == j){
+        auto PhiCol = getCol<scalar_t>(Phi,i);
+
+        //yp = y + 2.*eps*PhiCol;
+        ::pressio::ops::update(yp,0.,y,1.,PhiCol,2.*eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,0.,ftmp,-1.);
+
+        //yp = y + eps*PhiCol (or yp = yp - eps PhiCol);
+        ::pressio::ops::update(yp,1.,PhiCol,-eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,16.);
+
+    
+        ::pressio::ops::update(workingVector,1.,f0,-30.);
+    
+      
+        //yp = y - eps*PhiCol;
+        ::pressio::ops::update(yp,1.,PhiCol,-2.*eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,16.);
+       
+        //yp = y - 2.*eps*PhiCol;
+        ::pressio::ops::update(yp,1.,PhiCol,-eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+ 
+        ::pressio::ops::scale(workingVector,1./(12.*(eps*eps)));
+      }
+      else{
+        auto PhiCol_i = getCol<scalar_t>(Phi,i);
+        auto PhiCol_j = getCol<scalar_t>(Phi,j);
+
+        //yp = y + eps*PhiCol_i + eps*PhiCol_j;
+        ::pressio::ops::update(yp,0.,y,1.,PhiCol_i,eps,PhiCol_j,eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,0.,ftmp,1.);
+
+        //yp = y + eps*PhiCol_i - eps*PhiCol_j;
+        ::pressio::ops::update(yp,1.,PhiCol_j,-2.*eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+
+        //yp = y - eps*PhiCol_i + eps*PhiCol_j;
+        ::pressio::ops::update(yp,1.,PhiCol_i,-2.*eps,PhiCol_j,2.*eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,-1.);
+       
+        yp = y - eps*PhiCol_i - eps*PhiCol_j;
+        ::pressio::ops::update(yp,1.,PhiCol_j,-2.*eps);
+        ::pressio::ops::set_zero(ftmp);
+        appObj.velocity(yp,t,ftmp);
+        ::pressio::ops::update(workingVector,1.,ftmp,1.);
+ 
+        ::pressio::ops::scale(workingVector,0.25/(eps*eps));
+
+      }
+      ::pressio::ops::product(::pressio::transpose(),1., Phi,workingVector , 0., HCol);
+      for (int k = 0; k < romDim; k++){
+        BasisTransposeTimesHessianTimesBasisTimesBasis[k][i][j] = HCol(k);
+      } 
+    }
+  }
+  return BasisTransposeTimesHessianTimesBasisTimesBasis; 
+} 
+
+}}}
+#endif
