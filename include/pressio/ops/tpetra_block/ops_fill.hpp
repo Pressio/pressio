fd1b524b36a116050913bc04b3bfc0679e68dc6a
Francesco Rizzi <fnrizzi@sandia.gov>
2022-11-04 11:12:29 +0100

    complete #481

--- include/pressio/ops/tpetra_block/ops_fill.hpp
+++ include/pressio/ops/tpetra_block/ops_fill.hpp
@@ -52,17 +52,11 @@
 namespace pressio{ namespace ops{
 
 template <typename T, class ScalarType>
-#ifdef PRESSIO_ENABLE_CXX20
-requires (::pressio::is_vector_tpetra_block<T>::value
-       or ::pressio::is_multi_vector_tpetra_block<T>::value)
-       and std::convertible_to<ScalarType, typename ::pressio::Traits<T>::scalar_type>
-void
-#else
 ::pressio::mpl::enable_if_t<
-  ::pressio::is_vector_tpetra_block<T>::value or
-  ::pressio::is_multi_vector_tpetra_block<T>::value
+  (::pressio::is_vector_tpetra_block<T>::value ||
+   ::pressio::is_multi_vector_tpetra_block<T>::value)
+  && std::is_convertible<ScalarType, typename ::pressio::Traits<T>::scalar_type>::value
   >
-#endif
 fill(T & o, const ScalarType & value)
 {
   const typename ::pressio::Traits<T>::scalar_type v(value);
