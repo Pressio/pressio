9567e1259b92be521f3f06160e555778c7ad1d9f
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2022-11-29 20:32:38 +0100

    ops: epetra: add teuchos vector cases to level2 unit tests

--- tests/functional_small/ops/ops_epetra_level2.cc
+++ tests/functional_small/ops/ops_epetra_level2.cc
@@ -2,6 +2,69 @@
 #include "epetra_only_fixtures.hpp"
 #include "pressio/ops.hpp"
 
+TEST_F(epetraMultiVectorGlobSize15Fixture, mv_prod_teuchos_vector)
+{
+    for (int i=0; i<localSize_; ++i){
+     for (int j=0; j<numVecs_; ++j){
+        (*myMv_)[j][i] = (double)j;
+     }
+    }
+
+    Teuchos::SerialDenseVector<int, double> a(numVecs_);
+    a = 1.;
+
+    Epetra_Vector y(*contigMap_);
+    y.PutScalar(0.);
+    pressio::ops::product(::pressio::nontranspose{}, 1., *myMv_, a, 1., y);
+
+    EXPECT_DOUBLE_EQ(y[0], 6.);
+    EXPECT_DOUBLE_EQ(y[1], 6.);
+    EXPECT_DOUBLE_EQ(y[2], 6.);
+    EXPECT_DOUBLE_EQ(y[3], 6.);
+
+    // simulate beta=0 with uninitialized y containing NaN
+    y.PutScalar(std::nan("0"));
+    pressio::ops::product(::pressio::nontranspose{}, 1., *myMv_, a, 0., y);
+
+    EXPECT_DOUBLE_EQ(y[0], 6.);
+    EXPECT_DOUBLE_EQ(y[1], 6.);
+    EXPECT_DOUBLE_EQ(y[2], 6.);
+    EXPECT_DOUBLE_EQ(y[3], 6.);
+}
+
+TEST_F(epetraMultiVectorGlobSize15Fixture, mv_prod_storein_teuchos_vector)
+{
+    auto & myMv_h = *myMv_;
+    for (int i=0; i<localSize_; ++i){
+     for (int j=0; j<numVecs_; ++j){
+        myMv_h[j][i] = (double)i;
+     }
+    }
+
+    Epetra_Vector y(*contigMap_);
+    y.PutScalar(2.);
+
+    Teuchos::SerialDenseVector<int, double> a(numVecs_);
+    a = 1.;
+    pressio::ops::product(::pressio::transpose{}, 1., *myMv_, y, 1., a);
+
+    const auto a1_ref = numProc_ * 2. * 10. + 1.;
+    EXPECT_DOUBLE_EQ(a(0), a1_ref);
+    EXPECT_DOUBLE_EQ(a(1), a1_ref);
+    EXPECT_DOUBLE_EQ(a(2), a1_ref);
+    EXPECT_DOUBLE_EQ(a(3), a1_ref);
+
+    // simulate beta=0 with uninitialized y containing NaN
+    a = std::nan("0");
+    pressio::ops::product(::pressio::transpose{}, 1., *myMv_, y, 0., a);
+
+    const auto a0_ref = numProc_ * 2. * 10. + 0.;
+    EXPECT_DOUBLE_EQ(a(0), a0_ref);
+    EXPECT_DOUBLE_EQ(a(1), a0_ref);
+    EXPECT_DOUBLE_EQ(a(2), a0_ref);
+    EXPECT_DOUBLE_EQ(a(3), a0_ref);
+}
+
 #ifdef PRESSIO_ENABLE_TPL_EIGEN
 TEST_F(epetraMultiVectorGlobSize15Fixture, mv_prod_eigen_vector)
 {
