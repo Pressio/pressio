efa2253c1038f702dfbbca115487ea3b1785c47c
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2023-01-23 13:40:36 +0100

    ops: eigen: add rank-2 update expression test

--- tests/functional_small/ops/ops_eigen_matrix.cc
+++ tests/functional_small/ops/ops_eigen_matrix.cc
@@ -7,7 +7,7 @@ TEST(ops_eigen, dense_matrix_clone)
   using T = Eigen::MatrixXd;
   T a(6,8);
 
-  int c=0;  
+  int c=0;
   for (int i=0; i<6; ++i){
     for (int j=0; j<8; ++j){
      a(i,j)= (double) ++c;
@@ -110,7 +110,7 @@ TEST(ops_eigen, add_to_diagonal)
     for (int j=0; j<6; ++j){
       if (i==j) {
         EXPECT_DOUBLE_EQ(A(i,j), 3.3);
-      } 
+      }
       else{
         EXPECT_DOUBLE_EQ(A(i,j), 2.2);
       }
@@ -130,4 +130,23 @@ TEST(ops_eigen, dense_matrix_update)
   EXPECT_DOUBLE_EQ(M(0, 1), 22.);
   EXPECT_DOUBLE_EQ(M(1, 0), 27.);
   EXPECT_DOUBLE_EQ(M(1, 1), 32.);
+}
+
+TEST(ops_eigen, dense_matrix_update_epxr)
+{
+  Eigen::Matrix<double, 4, 4> M0;
+  Eigen::Matrix<double, 4, 4> A0;
+  pressio::ops::fill(M0, 1);
+  pressio::ops::fill(A0, 2);
+  auto M = pressio::subspan(M0, {1, 3}, {1, 3});
+  auto A = pressio::subspan(A0, {1, 3}, {1, 3});
+
+  pressio::ops::update(M, 2., A, 3.);
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      const bool sub = i > 0 && i < 3 && j > 0 && j < 3;
+      EXPECT_DOUBLE_EQ(M0(i, j), sub ? 8.   // updated M
+                                     : 1.); // unmodified part of M0
+    }
+  }
 }
