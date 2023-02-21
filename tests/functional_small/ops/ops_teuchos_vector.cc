56e538dfa090404fe214e5d36101be8719aec624
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2023-02-21 18:08:13 +0100

    ops: teuchos: add `clone` implementation and unit test

--- tests/functional_small/ops/ops_teuchos_vector.cc
+++ tests/functional_small/ops/ops_teuchos_vector.cc
@@ -4,6 +4,25 @@
 
 using V_t = Teuchos::SerialDenseVector<int, double>;
 
+TEST(ops_teuchos, vector_clone)
+{
+  const int n = 6;
+  V_t a(n);
+  for (int i = 0; i < 6; ++i){
+    a(i) = i + 1.;
+  }
+
+  auto b = pressio::ops::clone(a);
+  ASSERT_EQ(::pressio::ops::extent(b, 0), 6);
+  for (int i = 0; i < 6; ++i){
+    ASSERT_DOUBLE_EQ(b(i), a(i));
+  }
+
+  // check if b.data() == a.data()
+  a(0) = 55.;
+  ASSERT_DOUBLE_EQ(b(0), 1.);
+}
+
 TEST(ops_teuchos, vector_scale)
 {
   const int n = 6;
