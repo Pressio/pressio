0c46ca7c2a9790d6916f13020a757e85f4ca8fe8
Mikołaj Zuzek <mikolaj.zuzek@ng-analytics.com>
2023-02-27 20:29:49 +0100

    ops: tpetra-block: skip rank check in `dot`

--- include/pressio/ops/tpetra_block/ops_dot.hpp
+++ include/pressio/ops/tpetra_block/ops_dot.hpp
@@ -53,11 +53,8 @@ namespace pressio{ namespace ops{
 
 template <typename T1, typename T2>
 ::pressio::mpl::enable_if_t<
-  // dot common constraints
-     ::pressio::Traits<T1>::rank == 1
-  && ::pressio::Traits<T2>::rank == 1
   // TPL/container specific
-  && ::pressio::is_vector_tpetra_block<T1>::value
+     ::pressio::is_vector_tpetra_block<T1>::value
   && ::pressio::is_vector_tpetra_block<T2>::value
   // scalar compatibility
   && ::pressio::all_have_traits_and_same_scalar<T1, T2>::value
@@ -81,11 +78,8 @@ dot(const T1 & a, const T2 & b)
 
 template <typename T1, typename T2, class DotResult>
 ::pressio::mpl::enable_if_t<
-  // dot common constraints
-     ::pressio::Traits<T1>::rank == 1
-  && ::pressio::Traits<T2>::rank == 1
   // TPL/container specific
-  && ::pressio::is_vector_tpetra_block<T1>::value
+     ::pressio::is_vector_tpetra_block<T1>::value
   && ::pressio::is_vector_tpetra_block<T2>::value
   // scalar compatibility
   && ::pressio::all_have_traits_and_same_scalar<T1, T2>::value
