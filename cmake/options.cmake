6bb8896ae4286878d6ae95c26681837a4bd64b15
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2023-08-31 12:50:33 +0200

    move CMake options to a separate file

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,9 +1,27 @@
+option(PRESSIO_ENABLE_CXX14 "Enable C++14" OFF)
+option(PRESSIO_ENABLE_CXX17 "Enable C++17" OFF)
+option(PRESSIO_ENABLE_CXX20 "Enable C++20" OFF)
+
+if (CMAKE_CXX_STANDARD EQUAL 14)
+  add_definitions(-DPRESSIO_ENABLE_CXX14)
+elseif(CMAKE_CXX_STANDARD EQUAL 17)
+  add_definitions(-DPRESSIO_ENABLE_CXX17)
+elseif(CMAKE_CXX_STANDARD EQUAL 20)
+  add_definitions(-DPRESSIO_ENABLE_CXX20)
+endif()
+
+option(PRESSIO_ENABLE_DEBUG_PRINT "Enable debug printing" OFF)
+if (PRESSIO_ENABLE_DEBUG_PRINT)
+  add_definitions(-DPRESSIO_ENABLE_DEBUG_PRINT)
+endif()
 
 option(PRESSIO_ENABLE_TPL_EIGEN		"Enable Eigen TPL"	ON)
 option(PRESSIO_ENABLE_TPL_TRILINOS	"Enable Trilinos TPL"	OFF)
 option(PRESSIO_ENABLE_TPL_KOKKOS	"Enable Kokkos TPL"	OFF)
 option(PRESSIO_ENABLE_TPL_MPI		"Enable MPI"		OFF)
 option(PRESSIO_ENABLE_TPL_PYBIND11	"Enable Pybind11 TPL"	OFF)
+# option(PRESSIO_ENABLE_TEUCHOS_TIMERS "bla bla" OFF)
+
 
 if(PRESSIO_ENABLE_TPL_EIGEN)
   message(">> Eigen is currently enabled by default via PRESSIO_ENABLE_TPL_EIGEN=ON")
