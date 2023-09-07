de09f4f8991dc724e1dfefec70dca8c542e8718f
Francesco Rizzi <fnrizzi@sandia.gov>
2023-09-07 04:53:19 +0200

    fix CI error

--- include/pressio/ode/concepts/ode_system.hpp
+++ include/pressio/ode/concepts/ode_system.hpp
@@ -4,6 +4,7 @@
 
 #include "ode_predicates_for_system.hpp"
 #include "ode_has_const_discrete_residual_jacobian_method.hpp"
+#include "ode_policy_has_call_overload_for_userdefined_action_on_stencil.hpp"
 
 namespace pressio{ namespace ode{
 
@@ -337,30 +338,6 @@ struct ImplicitResidualJacobianPolicy<
       >::value
     >
   > : std::true_type{};
-
-
-template<class T>
-using policy_has_call_overload_for_userdefined_action_on_stencil_states_t =
-      decltype
-      (
-       std::declval<T const>()
-       (
-	std::declval<StencilStatesPotentiallyOverwrittenByUser>(),
-	std::declval<StepScheme const &>(),
-	std::declval<typename T::state_type const &>(),
-	std::declval<ImplicitStencilStatesDynamicContainer<typename T::state_type> const & >(),
-	std::declval<ImplicitStencilRightHandSideDynamicContainer<typename T::residual_type> & >(),
-	std::declval< ::pressio::ode::StepEndAt<typename T::independent_variable_type> >(),
-	std::declval< ::pressio::ode::StepCount >(),
-	std::declval< ::pressio::ode::StepSize<typename T::independent_variable_type> >(),
-	std::declval<typename T::residual_type &>(),
-#ifdef PRESSIO_ENABLE_CXX17
-	std::declval< std::optional<typename T::jacobian_type*> >()
-#else
-	std::declval< typename T::jacobian_type* >()
-#endif
-	)
-       );
 
 template<class T, class = void>
 struct ImplicitResidualJacobianPolicyForUserDefinedStencilStatesAction
