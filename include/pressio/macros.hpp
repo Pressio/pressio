2cad07da071d406ebdcabe58a5a866f2d6dae0bf
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2023-10-30 22:38:08 +0100

    use correct file name

--- include/pressio/macros.hpp
+++ include/pressio/macros.hpp
@@ -2,7 +2,7 @@
 //@HEADER
 // ************************************************************************
 //
-// pressio_cmake_config.h.in
+// macros.hpp
 //                     		  Pressio
 //                             Copyright 2019
 //    National Technology & Engineering Solutions of Sandia, LLC (NTESS)
